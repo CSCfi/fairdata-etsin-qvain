7d60ec7a03c7ac312fe9d4edf6eb2202133cbdf0
Aaron Hakala <aaron.hakala@metropolia.fi>
2017-10-04 06:58:14 +0300

    added nav icon for mobile

--- etsin_finder/frontend/js/components/navi.jsx
+++ etsin_finder/frontend/js/components/navi.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 
 export default class Navi extends React.Component {
+  openNavi(event) {
+    console.log(event.target.className);
+    event.target.className = event.target.className ? "" : "open";
+    let navList = document.querySelector(".nav-list");
+    if (navList.classList.contains("open")) {
+      navList.classList.remove("open");
+    } else {
+      navList.classList.add("open");
+    }
+  }
+
   render() {
     return (
-      <nav role="navigation" className="nav navigation">
-        <a href="#" className="nav-link active">Koti</a>
-        <a href="#" className="nav-link">Aineistot</a>
-        <a href="#" className="nav-link">Organisaatiot</a>
-        <a href="#" className="nav-link">Ohjeet ja Info</a>
-      </nav>
+      <div className="navigation">
+        <div id="nav-icon" onClick={this.openNavi}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <nav role="navigation" className="nav nav-list">
+          <a href="#" className="nav-link active">Koti</a>
+          <a href="#" className="nav-link">Aineistot</a>
+          <a href="#" className="nav-link">Organisaatiot</a>
+          <a href="#" className="nav-link">Ohjeet ja Info</a>
+        </nav>
+      </div>
     );
   }
 };
