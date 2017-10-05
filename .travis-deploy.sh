4f97a800d05b25f975a94618e90180832181e144
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-10-05 09:22:40 +0300

    Add Travis stuff.

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -xeu
 
-# Run as the after_success step of a travis build.
+# Run after the tests are successfully completed in travis build.
+
+if [[ "$TRAVIS_BRANCH" == "playground" || "$TRAVIS_BRANCH" == "master" || "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+    exit 0
+fi
 
 git clone https://github.com/CSCfi/etsin-ops
 cd etsin-ops/ansible/
@@ -8,12 +12,15 @@ cd etsin-ops/ansible/
 if [[ "$TRAVIS_BRANCH" == "test" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo "Deploying to test.."
     ansible-galaxy -r requirements.yml install --roles-path=roles
-    ansible-playbook -vv -i inventories/test/hosts site_deploy.yml --extra-vars "ssh_user=etsin-deploy-user"
+    ansible-playbook -vv -i inventories/test/hosts site_deploy.yml --extra-vars "ssh_user=etsin-user"
 elif [[ "$TRAVIS_BRANCH" == "stable" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    echo "Stable"
-elif [[ "$TRAVIS_BRANCH" == "staging" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    echo "Staging"
-elif [[ "$TRAVIS_BRANCH" == "production" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    echo "Production"
+    echo "Deploying to stable.. (NOT ACTUALLY, ADD CODE TO DO IT)"
 fi
 
+# Make sure the last command to run before this part is the ansible-playbook command
+if [ $? -eq 0 ]
+then
+    exit 0
+else
+    exit 1
+fi
