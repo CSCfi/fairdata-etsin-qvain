36faab46652e3c63493ea05c6733d4f089eea424
Meepu <meeri.panula@gofore.com>
2017-10-06 08:57:59 +0300

    CSCETSIN-73: Check if data is ready for display

--- etsin_finder/frontend/js/index.jsx
+++ etsin_finder/frontend/js/index.jsx
@@ -14,26 +14,14 @@ class FetchFromMetax extends React.Component {
     // TODO: Use Metax-test in dev env, actual Metax in production
     this.url = "https://metax-test.csc.fi"
 
-    // TODO: How many vars should we use to represent a dataset?
-    this.state = {
-      modified: "",
-      urn: ""
-    };
+    this.state = { dataset: [] };
   }
 
   componentDidMount() {
     axios.get(`${this.url}/rest/datasets/${this.identifier}.json`)
       .then(res => {
-
-        // TODO: Maybe just save the full dataset? Then create new subcomponents
-        // and let them handle picking out whatever piece they want to use.
-        // Right now I just want to print out some specific values to make sure I
-        // can access everything.
-        const modified = res.data.created_by_api;
-        this.setState({ modified });
-        const urn = res.data.research_dataset.urn_identifier;
-        this.setState({ urn });
-
+        const dataset = res.data;
+        this.setState({ dataset });
       })
       .catch(function (error) {
         console.log(error);
@@ -42,11 +30,18 @@ class FetchFromMetax extends React.Component {
 
   // TODO: All of this, obvs
   render() {
+    // Don't show anything until data has been loaded from Metax
+    // TODO: Use a loading indicator instead
+    // Do we need to worry about Metax sending us incomplete datasets?
+    if (!this.state.dataset.research_dataset) {
+      return <div></div>;
+    }
+
     return (
       <div>
         Here be important values:<br />
-        {this.state.modified}<br />
-        {this.state.urn}<br />
+        {this.state.dataset.created_by_api}<br />
+        {this.state.dataset.research_dataset.urn_identifier}<br />
       </div>
     );
   }
