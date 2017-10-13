91583804d239e27c3d69d3c63fedd0497426e6c2
Meepu <meeri.panula@gofore.com>
2017-10-13 09:37:42 +0300

    Fix merge conflicts

--- etsin_finder/frontend/js/index.jsx
+++ etsin_finder/frontend/js/index.jsx
@@ -9,26 +9,6 @@ import Content from "./layout/content";
 
 import stores from './stores';
 
-// Our translated strings
-import translations from '../locale/data.js';
-
-let localeData = translations();
-
-addLocaleData([...en, ...fi]);
-
-// Define user's language. Different browsers have the user locale defined
-// on different fields on the `navigator` object, so we make sure to account
-// for these different by checking all of them
-const language = (navigator.languages && navigator.languages[0]) ||
-                     navigator.language ||
-                     navigator.userLanguage;
-
-// Split locales with a region code
-const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
-
-// Try full locale, try locale without region code, fallback to 'en'
-const messages = localeData[languageWithoutRegionCode] || localeData[language] || localeData.en;
-
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
