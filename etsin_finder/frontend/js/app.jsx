cc6558b2228db473b10f45c9167571339e5561a7
Aaron Hakala <aaron.hakala@metropolia.fi>
2017-10-31 12:15:07 +0200

    CSCETSIN-87: made some further test setup

---
+++ etsin_finder/frontend/js/app.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'mobx-react';
+
+import Footer from './layout/footer';
+import Header from './layout/header';
+import Content from './layout/content';
+
+import Stores from './stores';
+
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'test') {
+  console.log('We are in test');
+} else if (process.env.NODE_ENV === 'development') {
+  console.log('We are in development');
+} else if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider Stores={Stores}>
+        <Router>
+          <div>
+            <Header />
+            <Content />
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
--- etsin_finder/frontend/js/index.jsx
+++ etsin_finder/frontend/js/index.jsx
@@ -1,37 +1,12 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'mobx-react';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 
-import Footer from './layout/footer';
-import Header from './layout/header';
-import Content from './layout/content';
+import App from './app'
 
-import Stores from './stores';
-
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'test') {
-  console.log('We are in test');
-} else if (process.env.NODE_ENV === 'development') {
-  console.log('We are in development');
-} else if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
-}
-
-class App extends Component {
+class Index extends Component {
   render() {
-    return (
-      <Provider Stores={Stores}>
-        <Router>
-          <div>
-            <Header />
-            <Content />
-            <Footer />
-          </div>
-        </Router>
-      </Provider>
-    );
+    return <App />
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('content'));
+ReactDOM.render(<Index />, document.getElementById('content'));
