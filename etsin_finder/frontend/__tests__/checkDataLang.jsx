b081dca8987fba02fd1650b084fa3c1ef48dc401
Aaron Hakala <aaron.hakala@metropolia.fi>
2017-11-15 15:06:20 +0200

    Wrote test for checkDataLang

---
+++ etsin_finder/frontend/__tests__/checkDataLang.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+
+import checkDataLang from '../js/utils/checkDataLang'
+
+describe('CheckDataLang function', () => {
+  const researchDataset = {publisher: {name: {en: 'English', fi: 'Finnish'}}}
+  let lang;
+  it('should return finnish for lang fi', () => {
+    lang = 'fi';
+    const checked = checkDataLang(researchDataset.publisher.name, lang)
+    expect(checked).toEqual('Finnish')
+  })
+  it('should return english for lang en', () => {
+    lang = 'en'
+    const checked = checkDataLang(researchDataset.publisher.name, lang)
+    expect(checked).toEqual('English')
+  })
+})
