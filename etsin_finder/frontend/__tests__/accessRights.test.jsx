e4b213111457ef3e78fa68d8fc5400dd13e0726f
Aaron Hakala <aaron.hakala@metropolia.fi>
2017-11-29 14:02:36 +0200

    fixed access rights test

--- etsin_finder/frontend/__tests__/accessRights.test.jsx
+++ etsin_finder/frontend/__tests__/accessRights.test.jsx
@@ -19,13 +19,13 @@ describe('AccessRights', () => {
     })
   })
   describe('render with access rights true', () => {
-    const accessRights = shallow(<AccessRights access_rights={ { type: [{identifier: 'http://purl.org/att/es/reference_data/access_type/access_type_open_access', label: {'fi': 'title'}}] } } Stores={{Locale: { currenLang: 'en' }}} />);
+    const accessRights = shallow(<AccessRights access_rights={ { type: [{identifier: 'http://purl.org/att/es/reference_data/access_type/access_type_open_access', pref_label: {'fi': 'title'}}] } } Stores={{Locale: { currenLang: 'en' }}} />);
     it('should render unclocked icon', () => {
       expect(accessRights.contains(<i className="fa fa-unlock" aria-hidden="true"></i>)).toEqual(true)
     })
   })
   describe('render with access rights anything else', () => {
-    const accessRights = shallow(<AccessRights access_rights={ { license: [{identifier: 'something', label: {'fi': 'title'}}] } } Stores={{Locale: { currenLang: 'en' }}} />);
+    const accessRights = shallow(<AccessRights access_rights={ { license: [{identifier: 'something', pref_label: {'fi': 'title'}}] } } Stores={{Locale: { currenLang: 'en' }}} />);
     it('should render locked icon', () => {
       expect(accessRights.contains(<i className="fa fa-lock" aria-hidden="true"></i>)).toEqual(true)
     })
