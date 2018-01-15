ad89bc8a2a59d8652bd70613996989fb0291bef9
Aaron Hakala <aaron.hakala@metropolia.fi>
2018-01-15 15:15:06 +0200

    CSCETSIN-126: search now searches all relevant fields

--- etsin_finder/frontend/js/utils/queryES.jsx
+++ etsin_finder/frontend/js/utils/queryES.jsx
@@ -2,12 +2,32 @@ import axios from 'axios'
 
 const queryES = (query) => {
   // Handle user search query
+
   let queryObject;
   if (query) {
     queryObject = {
       multi_match: {
         query,
-        fields: ['title.en', 'title.fi'], // todo
+        fields: [
+          'title.*',
+          'description.*',
+          'creator.name.*',
+          'contributor.name.*',
+          'publisher.name.*',
+          'rights_holder.name.*',
+          'curator.name.*',
+          'keyword',
+          'access_rights.license.pref_label.*',
+          'access_rights.type.identifier.*',
+          'access_rights.type.pref_label.*',
+          'theme.pref_label.*',
+          'field_of_science.pref_label.*',
+          'project.pref_label.*',
+          'urn_identifier',
+          'preferred_identifier',
+          'other_identifier.notation',
+          'other_identifier.type.pref_label.*',
+        ],
       },
     }
   } else {
