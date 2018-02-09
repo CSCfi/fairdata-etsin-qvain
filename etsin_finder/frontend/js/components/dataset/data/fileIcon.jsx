17246fca8c710d798877129f6107787a1dfadd4d
Aaron Hakala <aaron.hakala@metropolia.fi>
2018-02-09 11:18:16 +0200

    CSCETSIN-143: moved tooltip to own component

--- etsin_finder/frontend/js/components/dataset/data/fileIcon.jsx
+++ etsin_finder/frontend/js/components/dataset/data/fileIcon.jsx
@@ -4,43 +4,7 @@ import faFolder from '@fortawesome/fontawesome-free-regular/faFolder'
 import faFileVideo from '@fortawesome/fontawesome-free-regular/faFileVideo'
 import faFile from '@fortawesome/fontawesome-free-regular/faFile'
 import faFileAlt from '@fortawesome/fontawesome-free-regular/faFileAlt'
-import styled from 'styled-components'
-
-const Tooltip = styled.div`
-  display: inline-block;
-  position: relative;
-  color: inherit;
-  background-color: transparent;
-  &:before {
-    display: none;
-    content: "${props => props.title}";
-    padding: 3px 8px;
-    position: absolute;
-    bottom: 100%;
-    left: 50%;
-    transform: translate(-50%, -4px);
-    white-space: nowrap;
-    color: white;
-    background-color: #181818;
-    border-radius: 5px;
-  }
-  &:after {
-    display: none;
-    position: absolute;
-    bottom: 100%;
-    left: 50%;
-    transform: translate(-50%, 8px);
-    content: ' ';
-    border: 6px solid transparent;
-    border-top-color: #181818;
-  }
-  &:hover {
-    &:before,
-    &:after {
-      display: block;
-    }
-  }
-`
+import Tooltip from '../../general/tooltip'
 
 const FileIcon = props => {
   if (props.type === 'dir') {
