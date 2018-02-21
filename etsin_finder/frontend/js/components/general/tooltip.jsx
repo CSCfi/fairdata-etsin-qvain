4c1425f5e66d5310ca230421a1305dd7a5dc81bb
Aaron Hakala <aaron.hakala@metropolia.fi>
2018-02-21 15:53:33 +0200

    CSCETSIN-71: Improved tooltip

--- etsin_finder/frontend/js/components/general/tooltip.jsx
+++ etsin_finder/frontend/js/components/general/tooltip.jsx
@@ -1,39 +1,50 @@
+import React from 'react'
 import styled from 'styled-components'
 
-const Tooltip = styled.div`
+const Tip = styled.div.attrs({
+  bg: props => props.theme.color.darkgray,
+  fg: props => props.theme.color.white,
+})`
   display: inline-block;
   position: relative;
   color: inherit;
   background-color: transparent;
   &:before {
-    display: none;
+    font-size: 0.8em;
+    transition: all 0.3s ease;
+    opacity: 0;
     content: "${props => props.title}";
-    padding: 3px 8px;
+    padding: 0.1em 0.7em;
     position: absolute;
     bottom: 100%;
     left: 50%;
-    transform: translate(-50%, -4px);
+    transform: translate(-50%, -6px);
     white-space: nowrap;
-    color: white;
-    background-color: #181818;
+    color: ${props => props.fg};
+    background-color: ${props => props.bg};
     border-radius: 5px;
   }
   &:after {
-    display: none;
+    transition: all 0.3s ease;
+    opacity: 0;
     position: absolute;
     bottom: 100%;
     left: 50%;
-    transform: translate(-50%, 8px);
+    transform: translate(-50%, 6px);
     content: ' ';
     border: 6px solid transparent;
-    border-top-color: #181818;
+    border-top-color: ${props => props.bg};
   }
   &:hover {
     &:before,
     &:after {
-      display: block;
+      opacity: 1;
     }
   }
 `
-
+const Tooltip = props => (
+  <Tip {...props} aria-hidden="true">
+    {props.children}
+  </Tip>
+)
 export default Tooltip
