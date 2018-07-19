9c3342d7c8f34247af50712cf7ca7410d39239c4
Aaron Hakala <aaron.hakala@metropolia.fi>
2018-07-19 11:37:22 +0300

    CSCETSIN-223: fixed white flash.

--- etsin_finder/frontend/js/components/general/navigation/index.jsx
+++ etsin_finder/frontend/js/components/general/navigation/index.jsx
@@ -1,13 +1,13 @@
 {
-/**
- * This file is part of the Etsin service
- *
- * Copyright 2017-2018 Ministry of Education and Culture, Finland
- *
- *
- * @author    CSC - IT Center for Science Ltd., Espoo Finland <servicedesk@csc.fi>
- * @license   MIT
- */
+  /**
+   * This file is part of the Etsin service
+   *
+   * Copyright 2017-2018 Ministry of Education and Culture, Finland
+   *
+   *
+   * @author    CSC - IT Center for Science Ltd., Espoo Finland <servicedesk@csc.fi>
+   * @license   MIT
+   */
 }
 
 import React from 'react'
@@ -15,6 +15,7 @@ import { NavLink } from 'react-router-dom'
 import Translate from 'react-translate-component'
 import translate from 'counterpart'
 import styled from 'styled-components'
+import { Home, About, Search } from '../../../routes'
 
 import Accessibility from '../../../stores/view/accessibility'
 
@@ -25,6 +26,9 @@ export default class Navi extends React.Component {
         <NavItem
           exact
           to="/"
+          onPointerOver={() => {
+            Home.load()
+          }}
           onClick={() => {
             Accessibility.setNavText(translate('changepage', { page: translate('nav.datasets') }))
           }}
@@ -33,6 +37,9 @@ export default class Navi extends React.Component {
         </NavItem>
         <NavItem
           to="/datasets"
+          onPointerOver={() => {
+            Search.load()
+          }}
           onClick={() => {
             Accessibility.setNavText(translate('changepage', { page: translate('nav.datasets') }))
           }}
@@ -41,6 +48,9 @@ export default class Navi extends React.Component {
         </NavItem>
         <NavItem
           to="/about"
+          onPointerOver={() => {
+            About.load()
+          }}
           onClick={() => {
             Accessibility.setNavText(translate('changepage', { page: translate('nav.help') }))
           }}
