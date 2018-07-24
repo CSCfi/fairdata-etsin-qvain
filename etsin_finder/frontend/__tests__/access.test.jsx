b3f3b05c998f9ec5c7713119f6e83c970b64b0d2
Aaron Hakala <aaron.hakala@metropolia.fi>
2018-07-24 14:48:09 +0300

    CSCETSIN-173: added store for access_rights, added available timestamp component

---
+++ etsin_finder/frontend/__tests__/access.test.jsx
@@ -0,0 +1,357 @@
+import axios from 'axios'
+import Access from '../js/stores/view/access'
+import auth from '../js/stores/domain/auth'
+
+const accessTypes = {
+  open: 'http://purl.org/att/es/reference_data/access_type/access_type_open_access',
+  closed: 'http://purl.org/att/es/reference_data/access_type/access_type_closed_access',
+  embargoed: 'http://purl.org/att/es/reference_data/access_type/access_type_embargoed_access',
+  restricted_access:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access',
+  restricted_access_permit_fairdata:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access_permit_fairdata',
+  restricted_access_permit_external:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access_permit_external',
+  restricted_access_research:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access_research',
+  restricted_access_research_education_studying:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access_education_studying',
+  restricted_access_registration:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access_registration',
+}
+
+let accessRights
+
+describe('Access Store', () => {
+  it('get data', done => {
+    axios.get('https://metax-test.csc.fi/rest/datasets/13').then(response => {
+      accessRights = response.data.research_dataset.access_rights
+      done()
+    })
+  })
+  describe('Update for open access', () => {
+    it('Should start update process', done => {
+      Access.updateAccess(accessRights)
+      done()
+    })
+    it('Should show Remote files', () => {
+      expect(Access.restrictions.allowRemote).toEqual(true)
+    })
+    it('Should show IDA files', () => {
+      expect(Access.restrictions.allowDataIda).toEqual(true)
+    })
+    it('Should allow Remote file download', () => {
+      expect(Access.restrictions.allowRemoteDownload).toEqual(true)
+    })
+    it('Should allow Ida file download', () => {
+      expect(Access.restrictions.allowDataDownload).toEqual(true)
+    })
+    it('Should show Ida file info', () => {
+      expect(Access.restrictions.allowDataInfoButton).toEqual(true)
+    })
+    it('Should not show ask for access', () => {
+      expect(Access.restrictions.allowAskForAccess).toEqual(false)
+    })
+  })
+  describe('Update for closed access', () => {
+    it('Should start update process', done => {
+      const closed = accessRights
+      closed.access_type.identifier = accessTypes.closed
+      Access.updateAccess(closed)
+      done()
+    })
+    it('Should show Remote files', () => {
+      expect(Access.restrictions.allowRemote).toEqual(true)
+    })
+    it('Should allow Remote file download', () => {
+      expect(Access.restrictions.allowRemoteDownload).toEqual(true)
+    })
+    it('Should show IDA files', () => {
+      expect(Access.restrictions.allowDataIda).toEqual(true)
+    })
+    it('Should not allow Ida file download', () => {
+      expect(Access.restrictions.allowDataDownload).toEqual(false)
+    })
+    it('Should not show Ida file info', () => {
+      expect(Access.restrictions.allowDataInfoButton).toEqual(false)
+    })
+    it('Should not show ask for access', () => {
+      expect(Access.restrictions.allowAskForAccess).toEqual(false)
+    })
+  })
+  describe('Update for embargoed access', () => {
+    describe('Is available', () => {
+      it('Should start update process', done => {
+        const embargoed = accessRights
+        embargoed.access_type.identifier = accessTypes.embargoed
+        Access.updateAccess(embargoed)
+        done()
+      })
+      it('Should show Remote files', () => {
+        expect(Access.restrictions.allowRemote).toEqual(true)
+      })
+      it('Should allow Remote file download', () => {
+        expect(Access.restrictions.allowRemoteDownload).toEqual(true)
+      })
+      it('Should show IDA files', () => {
+        expect(Access.restrictions.allowDataIda).toEqual(true)
+      })
+      it('Should allow Ida file download', () => {
+        expect(Access.restrictions.allowDataDownload).toEqual(true)
+      })
+      it('Should show Ida file info', () => {
+        expect(Access.restrictions.allowDataInfoButton).toEqual(true)
+      })
+      it('Should not show ask for access', () => {
+        expect(Access.restrictions.allowAskForAccess).toEqual(false)
+      })
+    })
+    describe('Unavailable', () => {
+      it('Should start update process', done => {
+        const embargoed = accessRights
+        const d = new Date()
+        d.setFullYear(d.getFullYear() + 1)
+        embargoed.available = d.toISOString()
+        embargoed.access_type.identifier = accessTypes.embargoed
+        Access.updateAccess(embargoed)
+        done()
+      })
+      it('Should not show Remote files', () => {
+        expect(Access.restrictions.allowRemote).toEqual(false)
+      })
+      it('Should not allow Remote file download', () => {
+        expect(Access.restrictions.allowRemoteDownload).toEqual(false)
+      })
+      it('Should not show IDA files', () => {
+        expect(Access.restrictions.allowDataIda).toEqual(false)
+      })
+      it('Should not allow Ida file download', () => {
+        expect(Access.restrictions.allowDataDownload).toEqual(false)
+      })
+      it('Should not show Ida file info', () => {
+        expect(Access.restrictions.allowDataInfoButton).toEqual(false)
+      })
+      it('Should not show ask for access', () => {
+        expect(Access.restrictions.allowAskForAccess).toEqual(false)
+      })
+    })
+  })
+  describe('Update for restricted access', () => {
+    it('Should start update process', done => {
+      const restricted = accessRights
+      restricted.access_type.identifier = accessTypes.restricted_access
+      Access.updateAccess(restricted)
+      done()
+    })
+    it('Should show Remote files', () => {
+      expect(Access.restrictions.allowRemote).toEqual(true)
+    })
+    it('Should allow Remote file download', () => {
+      expect(Access.restrictions.allowRemoteDownload).toEqual(true)
+    })
+    it('Should show IDA files', () => {
+      expect(Access.restrictions.allowDataIda).toEqual(true)
+    })
+    it('Should not allow Ida file download', () => {
+      expect(Access.restrictions.allowDataDownload).toEqual(false)
+    })
+    it('Should not show Ida file info', () => {
+      expect(Access.restrictions.allowDataInfoButton).toEqual(false)
+    })
+    it('Should not show ask for access', () => {
+      expect(Access.restrictions.allowAskForAccess).toEqual(false)
+    })
+  })
+  describe('Update for restricted access permit fairdata', () => {
+    it('Should start update process', done => {
+      const permit_fairdata = accessRights
+      permit_fairdata.access_type.identifier = accessTypes.restricted_access_permit_fairdata
+      Access.updateAccess(permit_fairdata)
+      done()
+    })
+    it('Should show Remote files', () => {
+      expect(Access.restrictions.allowRemote).toEqual(true)
+    })
+    it('Should allow Remote file download', () => {
+      expect(Access.restrictions.allowRemoteDownload).toEqual(true)
+    })
+    it('Should show IDA files', () => {
+      expect(Access.restrictions.allowDataIda).toEqual(true)
+    })
+    it('Should not allow Ida file download', () => {
+      expect(Access.restrictions.allowDataDownload).toEqual(false)
+    })
+    it('Should not show Ida file info', () => {
+      expect(Access.restrictions.allowDataInfoButton).toEqual(false)
+    })
+    it('Should show ask for access', () => {
+      expect(Access.restrictions.allowAskForAccess).toEqual(true)
+    })
+  })
+  describe('Update for restricted access permit external', () => {
+    it('Should start update process', done => {
+      const permit_external = accessRights
+      permit_external.access_type.identifier = accessTypes.restricted_access_permit_external
+      Access.updateAccess(permit_external)
+      done()
+    })
+    it('Should show Remote files', () => {
+      expect(Access.restrictions.allowRemote).toEqual(true)
+    })
+    it('Should allow Remote file download', () => {
+      expect(Access.restrictions.allowRemoteDownload).toEqual(true)
+    })
+    it('Should show IDA files', () => {
+      expect(Access.restrictions.allowDataIda).toEqual(true)
+    })
+    it('Should not allow Ida file download', () => {
+      expect(Access.restrictions.allowDataDownload).toEqual(false)
+    })
+    it('Should not show Ida file info', () => {
+      expect(Access.restrictions.allowDataInfoButton).toEqual(false)
+    })
+    it('Should show ask for access', () => {
+      expect(Access.restrictions.allowAskForAccess).toEqual(true)
+    })
+  })
+  describe('Update for restricted access research', () => {
+    it('Should start update process', done => {
+      const restricted_research = accessRights
+      restricted_research.access_type.identifier = accessTypes.restricted_access_research
+      Access.updateAccess(restricted_research)
+      done()
+    })
+    it('Should show Remote files', () => {
+      expect(Access.restrictions.allowRemote).toEqual(true)
+    })
+    it('Should allow Remote file download', () => {
+      expect(Access.restrictions.allowRemoteDownload).toEqual(true)
+    })
+    it('Should show IDA files', () => {
+      expect(Access.restrictions.allowDataIda).toEqual(true)
+    })
+    it('Should not allow Ida file download', () => {
+      expect(Access.restrictions.allowDataDownload).toEqual(false)
+    })
+    it('Should not show Ida file info', () => {
+      expect(Access.restrictions.allowDataInfoButton).toEqual(false)
+    })
+    it('Should not show ask for access', () => {
+      expect(Access.restrictions.allowAskForAccess).toEqual(false)
+    })
+  })
+  describe('Update for restricted access research education studying', () => {
+    describe('User not logged in', () => {
+      it('Should start update process', done => {
+        auth.userLogged = false
+        const restricted_research_studying = accessRights
+        restricted_research_studying.access_type.identifier =
+          accessTypes.restricted_access_research_education_studying
+        Access.updateAccess(restricted_research_studying)
+        done()
+      })
+      it('Should show Remote files', () => {
+        expect(Access.restrictions.allowRemote).toEqual(true)
+      })
+      it('Should allow Remote file download', () => {
+        expect(Access.restrictions.allowRemoteDownload).toEqual(true)
+      })
+      it('Should show IDA files', () => {
+        expect(Access.restrictions.allowDataIda).toEqual(true)
+      })
+      it('Should not allow Ida file download', () => {
+        expect(Access.restrictions.allowDataDownload).toEqual(false)
+      })
+      it('Should not show Ida file info', () => {
+        expect(Access.restrictions.allowDataInfoButton).toEqual(false)
+      })
+      it('Should not show ask for access', () => {
+        expect(Access.restrictions.allowAskForAccess).toEqual(false)
+      })
+    })
+    describe('User logged in', () => {
+      it('Should start update process', done => {
+        auth.userLogged = true
+        const restricted_research_studying = accessRights
+        restricted_research_studying.access_type.identifier =
+          accessTypes.restricted_access_research_education_studying
+        Access.updateAccess(restricted_research_studying)
+        done()
+      })
+      it('Should show Remote files', () => {
+        expect(Access.restrictions.allowRemote).toEqual(true)
+      })
+      it('Should allow Remote file download', () => {
+        expect(Access.restrictions.allowRemoteDownload).toEqual(true)
+      })
+      it('Should show IDA files', () => {
+        expect(Access.restrictions.allowDataIda).toEqual(true)
+      })
+      it('Should not allow Ida file download', () => {
+        expect(Access.restrictions.allowDataDownload).toEqual(true)
+      })
+      it('Should not show Ida file info', () => {
+        expect(Access.restrictions.allowDataInfoButton).toEqual(true)
+      })
+      it('Should not show ask for access', () => {
+        expect(Access.restrictions.allowAskForAccess).toEqual(false)
+      })
+    })
+  })
+  describe('Update for restricted access registration', () => {
+    describe('User not logged in', () => {
+      it('Should start update process', done => {
+        auth.userLogged = false
+        const restricted_registration = accessRights
+        restricted_registration.access_type.identifier = accessTypes.restricted_access_registration
+        Access.updateAccess(restricted_registration)
+        done()
+      })
+      it('Should show Remote files', () => {
+        expect(Access.restrictions.allowRemote).toEqual(true)
+      })
+      it('Should allow Remote file download', () => {
+        expect(Access.restrictions.allowRemoteDownload).toEqual(true)
+      })
+      it('Should show IDA files', () => {
+        expect(Access.restrictions.allowDataIda).toEqual(true)
+      })
+      it('Should not allow Ida file download', () => {
+        expect(Access.restrictions.allowDataDownload).toEqual(false)
+      })
+      it('Should not show Ida file info', () => {
+        expect(Access.restrictions.allowDataInfoButton).toEqual(false)
+      })
+      it('Should not show ask for access', () => {
+        expect(Access.restrictions.allowAskForAccess).toEqual(false)
+      })
+    })
+    describe('User logged in', () => {
+      it('Should start update process', done => {
+        auth.userLogged = true
+        const restricted_registration = accessRights
+        restricted_registration.access_type.identifier = accessTypes.restricted_access_registration
+        Access.updateAccess(restricted_registration)
+        done()
+      })
+      it('Should show Remote files', () => {
+        expect(Access.restrictions.allowRemote).toEqual(true)
+      })
+      it('Should allow Remote file download', () => {
+        expect(Access.restrictions.allowRemoteDownload).toEqual(true)
+      })
+      it('Should show IDA files', () => {
+        expect(Access.restrictions.allowDataIda).toEqual(true)
+      })
+      it('Should not allow Ida file download', () => {
+        expect(Access.restrictions.allowDataDownload).toEqual(true)
+      })
+      it('Should not show Ida file info', () => {
+        expect(Access.restrictions.allowDataInfoButton).toEqual(true)
+      })
+      it('Should not show ask for access', () => {
+        expect(Access.restrictions.allowAskForAccess).toEqual(false)
+      })
+    })
+  })
+})
---
+++ etsin_finder/frontend/js/stores/view/access.js
@@ -0,0 +1,224 @@
+/**
+ * This file is part of the Etsin service
+ *
+ * Copyright 2017-2018 Ministry of Education and Culture, Finland
+ *
+ *
+ * @author    CSC - IT Center for Science Ltd., Espoo Finland <servicedesk@csc.fi>
+ * @license   MIT
+ */
+
+import { observable, action } from 'mobx'
+import auth from '../domain/auth'
+
+const accessTypes = {
+  open: 'http://purl.org/att/es/reference_data/access_type/access_type_open_access',
+  closed: 'http://purl.org/att/es/reference_data/access_type/access_type_closed_access',
+  embargoed: 'http://purl.org/att/es/reference_data/access_type/access_type_embargoed_access',
+  restricted_access:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access',
+  restricted_access_permit_fairdata:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access_permit_fairdata',
+  restricted_access_permit_external:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access_permit_external',
+  restricted_access_research:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access_research',
+  restricted_access_research_education_studying:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access_education_studying',
+  restricted_access_registration:
+    'http://purl.org/att/es/reference_data/access_type/access_type_restricted_access_registration',
+}
+
+class Access {
+  @observable
+  restrictions = {
+    allowDataIda: false,
+    allowRemote: false,
+    allowRemoteDownload: false,
+    allowDataInfoButton: false,
+    allowDataDownload: false,
+    allowAskForAccess: false,
+  }
+
+  updateAccess(access) {
+    switch (access.access_type.identifier) {
+      case accessTypes.open:
+        this.open()
+        break
+      case accessTypes.closed:
+        this.closed()
+        break
+      case accessTypes.embargoed:
+        this.embargoed(access.available)
+        break
+      case accessTypes.restricted_access:
+        this.restrictedAccess()
+        break
+      case accessTypes.restricted_access_permit_external:
+        this.restrictedAccessPermitExternal()
+        break
+      case accessTypes.restricted_access_permit_fairdata:
+        this.restrictedAccessPermitFairdata()
+        break
+      case accessTypes.restricted_access_registration:
+        this.restrictedAccessRegistration()
+        break
+      case accessTypes.restricted_access_research:
+        this.restrictedAccessResearch()
+        break
+      case accessTypes.restricted_access_research_education_studying:
+        this.restrictedAccessResearchEducationStudying()
+        break
+      default:
+        this.closed()
+    }
+  }
+
+  @action
+  open() {
+    this.restrictions = {
+      allowRemote: true,
+      allowRemoteDownload: true,
+      allowDataIda: true,
+      allowDataInfoButton: true,
+      allowDataDownload: true,
+      allowAskForAccess: false,
+    }
+  }
+
+  @action
+  closed() {
+    this.restrictions = {
+      allowRemote: true,
+      allowRemoteDownload: true,
+      allowDataIda: true,
+      allowDataInfoButton: false,
+      allowDataDownload: false,
+      allowAskForAccess: false,
+    }
+  }
+
+  @action
+  embargoed(av) {
+    if (new Date(av).getTime() < new Date().getTime()) {
+      this.restrictions = {
+        allowRemote: true,
+        allowRemoteDownload: true,
+        allowDataIda: true,
+        allowDataInfoButton: true,
+        allowDataDownload: true,
+        allowAskForAccess: false,
+      }
+    } else {
+      this.restrictions = {
+        allowRemote: false,
+        allowRemoteDownload: false,
+        allowDataIda: false,
+        allowDataInfoButton: false,
+        allowDataDownload: false,
+        allowAskForAccess: false,
+      }
+    }
+  }
+
+  @action
+  restrictedAccess() {
+    this.restrictions = {
+      allowRemote: true,
+      allowRemoteDownload: true,
+      allowDataIda: true,
+      allowDataInfoButton: false,
+      allowDataDownload: false,
+      allowAskForAccess: false,
+    }
+  }
+
+  @action
+  restrictedAccessPermitFairdata() {
+    // TODO: check if user has permission
+    // these are the default permissions
+    // this can not be checked yet
+    this.restrictions = {
+      allowRemote: true,
+      allowRemoteDownload: true,
+      allowDataIda: true,
+      allowDataInfoButton: false,
+      allowDataDownload: false,
+      allowAskForAccess: true,
+    }
+  }
+
+  @action
+  restrictedAccessPermitExternal() {
+    this.restrictions = {
+      allowRemote: true,
+      allowRemoteDownload: true,
+      allowDataIda: true,
+      allowDataInfoButton: false,
+      allowDataDownload: false,
+      allowAskForAccess: true,
+    }
+  }
+
+  @action
+  restrictedAccessResearch() {
+    this.restrictions = {
+      allowRemote: true,
+      allowRemoteDownload: true,
+      allowDataIda: true,
+      allowDataInfoButton: false,
+      allowDataDownload: false,
+      allowAskForAccess: false,
+    }
+  }
+
+  @action
+  restrictedAccessResearchEducationStudying() {
+    // TODO: Check login
+    if (auth.userLogged) {
+      this.restrictions = {
+        allowRemote: true,
+        allowRemoteDownload: true,
+        allowDataIda: true,
+        allowDataInfoButton: true,
+        allowDataDownload: true,
+        allowAskForAccess: false,
+      }
+    } else {
+      this.restrictions = {
+        allowRemote: true,
+        allowRemoteDownload: true,
+        allowDataIda: true,
+        allowDataInfoButton: false,
+        allowDataDownload: false,
+        allowAskForAccess: false,
+      }
+    }
+  }
+
+  @action
+  restrictedAccessRegistration() {
+    // TODO: Check login
+    if (auth.userLogged) {
+      this.restrictions = {
+        allowRemote: true,
+        allowRemoteDownload: true,
+        allowDataIda: true,
+        allowDataInfoButton: true,
+        allowDataDownload: true,
+        allowAskForAccess: false,
+      }
+    } else {
+      this.restrictions = {
+        allowRemote: true,
+        allowRemoteDownload: true,
+        allowDataIda: true,
+        allowDataInfoButton: false,
+        allowDataDownload: false,
+        allowAskForAccess: false,
+      }
+    }
+  }
+}
+
+export default new Access()
