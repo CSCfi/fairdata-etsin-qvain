9c5d18a8fb3c1d4ef5cda219e3183ae6414b3094
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2018-10-09 17:11:53 +0300

    CSCETSIN-291: [FIX] Fix test

--- etsin_finder/frontend/__tests__/dateFormat.test.jsx
+++ etsin_finder/frontend/__tests__/dateFormat.test.jsx
@@ -10,8 +10,4 @@ describe('check date', () => {
     const dateformat = dateFormat(1516007145830)
     expect(dateformat).toEqual('January 15, 2018, 11:05 AM')
   })
-  it('should return Finnish time with -00:00 timezone-part', () => {
-    const dateformat = dateFormat('2000-01-01T00:00:00-00:00')
-    expect(dateformat).toEqual('January 1, 2000, 2:00 AM')
-  })
 })
