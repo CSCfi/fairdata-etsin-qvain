208386466f9e3147464213da7227111a1477c4ba
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2018-10-09 17:03:06 +0300

    CSCETSIN-290: [REF] Add handling of hours minutes seconds in timestamps. Handle special case of timestamp having -00:00.

--- etsin_finder/frontend/__tests__/dateFormat.test.jsx
+++ etsin_finder/frontend/__tests__/dateFormat.test.jsx
@@ -6,8 +6,12 @@ describe('check date', () => {
     const dateformat = dateFormat('2017')
     expect(dateformat).toEqual(2017)
   })
-  it('should contain the translated (finnish) date', () => {
+  it('should change into human-readable datetime', () => {
     const dateformat = dateFormat(1516007145830)
-    expect(dateformat).toEqual('January 15, 2018')
+    expect(dateformat).toEqual('January 15, 2018, 11:05 AM')
+  })
+  it('should return Finnish time with -00:00 timezone-part', () => {
+    const dateformat = dateFormat('2000-01-01T00:00:00-00:00')
+    expect(dateformat).toEqual('January 1, 2000, 2:00 AM')
   })
 })
