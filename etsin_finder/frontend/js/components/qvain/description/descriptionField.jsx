77cab5a7ba86d7a845efc3c0be21172932d80df2
Kasper Keinänen <kasper.keinanen@csc.fi>
2019-04-09 13:29:04 +0300

    CSCETSIN-398:
    Added base version of description field to qvain.

---
+++ etsin_finder/frontend/js/components/qvain/description/descriptionField.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+class DescriptionField extends Component {
+  state = {
+    active: 'ENGLISH'
+  }
+
+  handleLanguageButtonClisck = () => {
+    /* eslint-disable no-unused-expressions */
+    this.state.active === 'ENGLISH'
+      ? this.setState({ active: 'FINNISH' })
+      : this.setState({ active: 'ENGLISH' })
+  }
+
+  render() {
+    return (
+      <div>
+        <LangButtonContainer>
+          {this.state.active === 'ENGLISH'
+            ? <LangButton active onClick={this.handleLanguageButtonClisck}>ENGLISH</LangButton>
+            : <LangButton onClick={this.handleLanguageButtonClisck}>ENGLISH</LangButton>
+          }
+          <EmptyBlock width="2%" />
+          {this.state.active === 'FINNISH'
+            ? <LangButton active onClick={this.handleLanguageButtonClisck}>FINNISH</LangButton>
+            : <LangButton onClick={this.handleLanguageButtonClisck}>FINNISH</LangButton>
+          }
+          <EmptyBlock width="48%" />
+        </LangButtonContainer>
+        <DescriptionCard>
+          <h3>Title</h3>
+          <Input
+            type="text"
+            placeholder="Title (English)"
+          />
+          <h3>Description</h3>
+          <Textarea rows="8" />
+          <div>Only one launguage selection is mandatory</div>
+        </DescriptionCard>
+      </div>
+    )
+  }
+}
+const DescriptionCard = styled.div`
+  margin-bottom: 15px;
+  padding: 25px 44px;
+  border: 1px solid #007fad;
+  border-top: none;
+  min-height: 150px;
+  background-color: #fff;
+  overflow: auto;
+`
+
+const LangButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+`
+const LangButton = styled.div`
+  width: 25%;
+  padding: 5px 20px;
+  background-color: #fff;
+  border: 1px solid ${props => (props.active ? '#007fad' : '#eceeef')};
+  border-bottom: ${props => (props.active ? 'none' : '1px solid #007fad')};
+  border-radius: 4px 4px 0 0;
+`
+const EmptyBlock = styled.div`
+  width: ${props => props.width};
+  border-bottom: 1px solid #007fad;
+`
+const Input = styled.input`
+  width: 100%;
+  border-radius: 3px;
+  border: 1px solid #eceeef;
+  padding: 8px;
+  color: #808080;
+  margin-bottom: 20px;
+`
+
+const Textarea = styled.textarea`
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  width: 100%;
+  border-radius: 4px;
+  border: solid 1px #cccccc;
+`
+
+
+export default DescriptionField;
--- etsin_finder/frontend/js/components/qvain/description/index.jsx
+++ etsin_finder/frontend/js/components/qvain/description/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import DescriptionFeild from './descriptionField';
 import OtherIdentifierField from './otherIdentifierField';
 import FieldOfScienceField from './fieldOfScienceField';
 import KeywordsField from './keywordsField';
@@ -8,6 +9,7 @@ const Description = () => (
   <div className="container">
     <h2>DESCRIPTION</h2>
     <div>
+      <DescriptionFeild />
       <OtherIdentifierField />
       <FieldOfScienceField />
       <KeywordsField />
