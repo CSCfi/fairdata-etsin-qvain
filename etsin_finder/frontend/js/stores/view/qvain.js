bc770b8f9048dd61a8333e34f14e2ce50ef84ae2
Otto Uusihakala <otto.uusihakala@gofore.com>
2019-04-09 14:08:25 +0300

    CSCETSIN-398 state management for field of science and other identifiers fields

--- etsin_finder/frontend/js/stores/view/qvain.js
+++ etsin_finder/frontend/js/stores/view/qvain.js
@@ -1,6 +1,10 @@
 import { observable, action } from 'mobx'
 
 class Qvain {
+  @observable otherIdentifiers = []
+
+  @observable fieldOfScience = {}
+
   @observable keywords = []
 
   @observable license = {}
@@ -8,8 +12,17 @@ class Qvain {
   @observable accessType = {}
 
   @action
+  addOtherIdentifier = (identifier) => {
+    this.otherIdentifiers = [...this.otherIdentifiers, identifier]
+  }
+
+  @action
+  setFieldOfScience = (fieldOfScience) => {
+    this.fieldOfScience = fieldOfScience
+  }
+
+  @action
   setKeywords = (keywords) => {
-    console.log('setKeywords: ', keywords)
     this.keywords = keywords
   }
 
