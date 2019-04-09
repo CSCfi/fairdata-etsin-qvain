09ae68a729ea766520d90337a5335ed2132483f3
Kasper Keinänen <kasper.keinanen@csc.fi>
2019-04-09 14:22:20 +0300

    CSCETSIN-398:
    Modified Keywords component, now stores keywords in local state (will be moved to mobex)

--- etsin_finder/frontend/js/components/qvain/description/keywordsField.jsx
+++ etsin_finder/frontend/js/components/qvain/description/keywordsField.jsx
@@ -1,19 +1,54 @@
-import React from 'react'
-import Select from 'react-select';
+import React, { Component } from 'react'
+import styled from 'styled-components'
 
 import Card from '../general/card';
 
-const KeywordsField = () => (
-  <Card>
-    <h3>Keywords</h3>
-    <Select
-      isMulti
-      name="keywords"
-      placeholder="E.g. Economy"
-      className="basic-multi-select"
-      classNamePrefix="select"
-    />
-  </Card>
-)
+class KeywordsField extends Component {
+  state = {
+    keywords: [],
+    value: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  handleKeywordAdd = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      const newKeyword = this.state.value;
+      this.setState(prevState => ({
+        keywords: [...prevState.keywords, newKeyword]
+      }))
+      this.setState({ value: '' })
+    }
+  }
+
+  render() {
+    const keywords = this.state.keywords.map(word => (<div>{word}</div>))
+    return (
+      <Card>
+        <h3>Keywords</h3>
+        {keywords}
+        <Input
+          value={this.state.value}
+          onChange={this.handleChange}
+          onKeyDown={this.handleKeywordAdd}
+          type="text"
+          placeholder="Title (English)"
+        />
+      </Card>
+    )
+  }
+}
+
+const Input = styled.input`
+  width: 100%;
+  border-radius: 3px;
+  border: 1px solid #eceeef;
+  padding: 8px;
+  color: #808080;
+  margin-bottom: 20px;
+`
 
 export default KeywordsField;
