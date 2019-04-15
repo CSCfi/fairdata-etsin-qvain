90634bd6c7e8e9e92abe570c658b629774f5f30b
Kasper Keinänen <kasper.keinanen@csc.fi>
2019-04-15 15:32:50 +0300

    CSCETSIN-398:
    Get reference data from metax when component renders.

--- etsin_finder/frontend/js/components/qvain/description/fieldOfScienceField.jsx
+++ etsin_finder/frontend/js/components/qvain/description/fieldOfScienceField.jsx
@@ -2,20 +2,54 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { inject, observer } from 'mobx-react'
 import Select from 'react-select';
-import Translate from 'react-translate-component'
+import Translate from 'react-translate-component';
 
+import getReferenceData from '../utils/getReferenceData';
 import Card from '../general/card';
-
-const scienceOptions = [
-  { value: 'PHYSICS', label: 'Physics', labelFi: 'Fysiikka' },
-  { value: 'MEDICINE', label: 'Medicine', labelFi: 'Lääketiede' }
-]
-
-const scienceOptionsFi = scienceOptions.map(option => ({ ...option, label: option.labelFi }))
 
 class FieldOfScienceField extends React.Component {
   static propTypes = {
     Stores: PropTypes.object.isRequired
+  }
+
+  state = {
+    fieldOfScienceEn: [{ value: '', label: '' }],
+    fieldOfScienceFi: [{ value: '', label: '' }]
+  }
+
+  componentDidMount = () => {
+    getReferenceData('field_of_science')
+    .then(res => {
+      const list = res.data.hits.hits;
+      const refsEn = list.map(ref => (
+        {
+          value: ref._source.label.en,
+          label: ref._source.label.en,
+        }
+        ))
+      const refsFi = list.map(ref => (
+        {
+          value: ref._source.label.en,
+          label: ref._source.label.fi,
+        }
+        ))
+      this.setState({ fieldOfScienceEn: refsEn })
+      this.setState({ fieldOfScienceFi: refsFi })
+    })
+    .catch(error => {
+      if (error.response) {
+        // Error response from Metax
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // No response from Metax
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+    });
   }
 
   render() {
@@ -28,7 +62,11 @@ class FieldOfScienceField extends React.Component {
           attributes={{ placeholder: 'qvain.description.fieldOfScience.placeholder' }}
           className="basic-single"
           classNamePrefix="select"
-          options={this.props.Stores.Locale.lang === 'en' ? scienceOptions : scienceOptionsFi}
+          options={
+            this.props.Stores.Locale.lang === 'en'
+            ? this.state.fieldOfScienceEn
+            : this.state.fieldOfScienceFi
+          }
           onChange={(fieldOfScience) => {
             this.props.Stores.Qvain.setFieldOfScience(fieldOfScience)
           }}
