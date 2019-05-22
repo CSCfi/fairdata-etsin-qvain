9354ffeb5a67cc5719e558671cee827c5adc0e2f
Tommi Pulli <topulli@h1040g2l-450ydf.x.csc.fi>
2019-05-22 16:15:28 +0300

    CSCETSIN-417:
    Dropdown menu with correct links implemented. Menu-button does not
    yet have any text and it only works on full size screen.

--- etsin_finder/frontend/js/components/general/navigation/settings.jsx
+++ etsin_finder/frontend/js/components/general/navigation/settings.jsx
@@ -18,6 +18,7 @@ import styled from 'styled-components'
 import Login from './loginButton'
 import { Link } from '../button'
 import LangToggle from './langToggle'
+import DropdownMenu from './dropdownMenu'
 // import { VerticalSeparator } from '../separator'
 
 export default class Settings extends Component {
@@ -34,15 +35,30 @@ export default class Settings extends Component {
           >
             <Translate content="nav.help" />
           </Link>
-          <Link
-            noMargin
-            width="max-content"
-            href="https://qvain.fairdata.fi"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <Translate content="nav.addDataset" />
-          </Link>
+          <DropdownMenu >
+          <CustomContainer>
+              <Row>
+                <Link
+                  width="100%"
+                  margin="0.4em 0em 0.4em 0.4em"
+                  href="https://qvain.fairdata.fi"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >Qvain
+                </Link>
+              </Row>
+              <Row>
+                <Link
+                  width="100%"
+                  margin="0.4em 0em 0.4em 0.4em"
+                  href="https://etsin.fairdata.fi/qvain"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >Qvain Lite
+                </Link>
+              </Row>
+            </CustomContainer>
+          </DropdownMenu>
           <LangToggle margin="0em 0em 0em 0.4em" />
           <Login />
         </Positioner>
@@ -50,6 +66,7 @@ export default class Settings extends Component {
     )
   }
 }
+
 
 Settings.defaultProps = {
   helpUrl: undefined,
@@ -64,4 +81,14 @@ const Positioner = styled.div`
   @media screen and (min-width: ${p => p.theme.breakpoints.lg}) {
     display: inline-flex;
   }
+`
+const CustomContainer = styled.div`
+  margin: 0 auto;
+  padding: 1em 1.3em;
+  max-width: 300px;
+  width: 100%;
+`
+const Row = styled.div`
+  display: inline-flex;
+  width: 100%;
 `
