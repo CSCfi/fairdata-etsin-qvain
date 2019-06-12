500eb1d63f5a96cef278b40ed07b8998fb95e0f3
Kasper Keinänen <kasper.keinanen@csc.fi>
2019-06-12 16:00:01 +0300

    CSCETSIN-381: Better error message for determining data_catalog.

--- etsin_finder/qvain_light_resources.py
+++ etsin_finder/qvain_light_resources.py
@@ -180,7 +180,7 @@ class QvainDataset(Resource):
         elif all(["remote_resources" not in data, "files" in data or "directorys" in data]):
             data_catalog = "urn:nbn:fi:att:data-catalog-ida"
         else:
-            return "Missing fields to specify the data catalog.", 400
+            return "Error specifying the datacatalog. Make shure you have added EITHER Ida files OR external files.", 400
         metax_redy_data = data_to_metax(data, metadata_provider_org, metadata_provider_user, data_catalog)
         metax_response = create_dataset(metax_redy_data)
         return metax_response, 200
@@ -215,7 +215,7 @@ class QvainDataset(Resource):
         elif all(["remote_resources" not in data, "files" in data or "directorys" in data]):
             data_catalog = "urn:nbn:fi:att:data-catalog-ida"
         else:
-            return "Missing fields to specify the data catalog.", 400
+            return "Error specifying the datacatalog. Make shure you have added EITHER Ida files OR external files.", 400
         metax_redy_data = data_to_metax(data, metadata_provider_org, metadata_provider_user, data_catalog)
         metax_response = update_dataset(metax_redy_data)
         return metax_response, 200
