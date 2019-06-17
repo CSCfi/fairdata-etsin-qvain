7ce7075dcdef8dcee498dbcedc93cb8eca9b8c06
Kasper Keinänen <kasper.keinanen@csc.fi>
2019-06-17 18:29:58 +0300

    Added parsing of the file and directory data befor sending to backend.

--- etsin_finder/frontend/js/components/qvain/utils/handleSubmit.js
+++ etsin_finder/frontend/js/components/qvain/utils/handleSubmit.js
@@ -11,6 +11,27 @@ const participantsToMetax = participants => {
   return parsedParticipant
 }
 
+const filesToMetax = files => {
+  const parsedFileData = files.map(file => ({
+    identifier: file.identifier,
+    title: file.title,
+    description: file.description ? file.description : undefined,
+    fileType: file.fileType ? file.fileType : undefined,
+    useCategory: file.useCategory
+  }))
+  return parsedFileData
+}
+
+const directorysToMetax = directorys => {
+  const parsedDirectoryData = directorys.map(dir => ({
+    identifier: dir.identifier,
+    title: dir.title,
+    description: dir.description ? dir.description : undefined,
+    useCategory: dir.useCategory
+  }))
+  return parsedDirectoryData
+}
+
 const handleSubmitToBackend = (values) => {
   const obj = {
     title: {
@@ -33,8 +54,8 @@ const handleSubmitToBackend = (values) => {
     // Send no values if empty instead of empty values.
     remote_resources:
       values._externalResources.length > 0 ? values._externalResources : undefined,
-    files: values._selectedFiles.idaGroups ? values._selectedFiles : undefined,
-    directorys: values._selectedDirectories.idaGroups ? values._selectedDirectories : undefined,
+    files: values._selectedFiles ? filesToMetax(values._selectedFiles) : undefined,
+    directorys: values._selectedDirectories ? directorysToMetax(values._selectedDirectories) : undefined,
   }
   return obj
 }
