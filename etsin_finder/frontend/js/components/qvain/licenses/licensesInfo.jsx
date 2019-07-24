295c5e1d7592f6ee60b110baadfc1525b673ad13
Kasper Keinänen <38975517+kasperkeinanen@users.noreply.github.com>
2019-07-24 11:17:29 +0300

    Cscetsin 538 add tooltips (#271)
    * CSCETSIN-538: Created General tooltip component.
    
    - Created a general tooltip component that better suits Qvain-light
      than the one found in Etsin.
    - Created new stateless componet for tooltips.
    - Effects using tooltips in Qvain-light.
    
    * CSCETSIN-538: Refactored Files tooltip.
    
    - Added new tooltip that functions with click or key events.
    - Wrapped the HelpIcon with the tooltip and refactored the icon to be
      more accessible.
    - Effects only the Files header.
    
    * CSCETSIN-538: Add info text components.
    
    - Added info text as components to the Qvain Light tooltips.
    - Created stateless components for the content of the info tooltips.
    - Effects info tooltips in Qvain Light.
    
    * Add eslint-plugin-react-hooks to etsin.
    
    - Added the eslint plugin to help use react hooks as intended.
    - Added the plugin to package.json and .eslintrc
    - Effects javascript linting in etsin.
    
    * CSCETSIN-538: Add handleClickOutside for Tooltip.
    
    - Handle clicks outside tooltip to close it. This is the expected
      behavior of this type of component.
    - Added click event listeners to the tooltip component that fire when
      there are clicks outside the wrapper of the child component, here the
      HelpIcon component.
    - Effects the Tooltip component and its child components.

---
+++ etsin_finder/frontend/js/components/qvain/licenses/licensesInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Translate from 'react-translate-component'
+
+const LicensesInfo = () => (
+  <>
+    <Translate component="h2" content="qvain.rightsAndLicenses.infoTitle" />
+    <Translate component="h3" content="qvain.rightsAndLicenses.license.title" />
+    <Translate component="p" content="qvain.rightsAndLicenses.license.infoText" />
+    <Translate component="h3" content="qvain.rightsAndLicenses.accessType.title" />
+    <Translate component="p" content="qvain.rightsAndLicenses.accessType.infoText" />
+  </>
+)
+
+export default LicensesInfo
