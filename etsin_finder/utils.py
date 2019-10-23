4f358e1dea65d9c6b894e0a2e7414ba92bed314d
Kasper Keinänen <kasper.keinanen@csc.fi>
2019-10-23 11:15:16 +0300

    CSCFAIRMETA-102: Added saml attributes to utils.
    - simplifies the use of the saml attributes and keeps the code DRY.
    - Added as constants.
    - no side effects.

--- etsin_finder/utils.py
+++ etsin_finder/utils.py
@@ -10,7 +10,6 @@
 import json
 import os
 from datetime import datetime
-
 import pytz
 from dateutil import parser
 
@@ -22,6 +21,15 @@ ACCESS_TYPES = {
     'restricted': 'http://uri.suomi.fi/codelist/fairdata/access_type/code/restricted'
 }
 
+SAML_ATTRIBUTES = {
+    'first_name': 'urn:oid:2.5.4.42',
+    'last_name': 'urn:oid:2.5.4.4',
+    'email': 'urn:oid:0.9.2342.19200300.100.1.3',
+    'haka_id': 'urn:oid:1.3.6.1.4.1.5923.1.1.1.6',
+    'haka_org_id': 'urn:oid:1.3.6.1.4.1.25178.1.2.9',
+    'CSC_username': 'urn:oid:1.3.6.1.4.1.16161.4.0.53',
+    'idm_groups': 'urn:oid:1.3.6.1.4.1.8057.2.80.26'
+}
 
 def executing_travis():
     """Returns True whenever code is being executed by travis"""
