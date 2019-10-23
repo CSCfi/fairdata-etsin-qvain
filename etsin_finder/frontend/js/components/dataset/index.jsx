40dd3f89ae9983322febe5b9cee68a7c1f8e7d30
Katri Tegel <katri.tegel@gmail.com>
2019-10-23 13:49:49 +0300

    CSCETSIN-703:Change the banner that shows whether dataset is removed/deprecated
    Bug fix:  Modified the index.jsx to fetch all version datasets to the constructor via Stores so the 'deprecated' information could be fetched from the version datasets directly ('deprecated' doesn't show in version_set like 'removed').
    
    Now the banner shows whether the dataset has been removed or deprecated, and if there is an existing version it will provide a link to it.
    
    Should not have side-effects

--- etsin_finder/frontend/js/components/dataset/index.jsx
+++ etsin_finder/frontend/js/components/dataset/index.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable space-before-function-paren */
 {
   /**
    * This file is part of the Etsin service
@@ -9,6 +11,7 @@
    * @license   MIT
    */
 }
+require('@babel/polyfill')
 
 import React from 'react'
 import PropTypes from 'prop-types'
@@ -18,6 +21,7 @@ import Translate from 'react-translate-component'
 import { inject, observer } from 'mobx-react'
 import { NavLink } from 'react-router-dom'
 
+import axios from 'axios'
 import DatasetQuery from '../../stores/view/datasetquery'
 import Accessibility from '../../stores/view/accessibility'
 import Sidebar from './sidebar'
@@ -26,6 +30,7 @@ import ErrorPage from '../errorpage'
 import ErrorBoundary from '../general/errorBoundary'
 import NoticeBar from '../general/noticeBar'
 import Loader from '../general/loader'
+
 
 const BackButton = styled(NavLink)`
   color: ${props => props.theme.color.primary};
@@ -42,16 +47,19 @@ class Dataset extends React.Component {
       email_info: DatasetQuery.email_info,
       error: false,
       identifier: props.match.params.identifier,
+      versionInfo: {},
     }
 
     this.query = this.query.bind(this)
     this.goBack = this.goBack.bind(this)
   }
 
+
   componentDidMount() {
     Accessibility.resetFocus()
     this.query()
   }
+
 
   // eslint-disable-next-line camelcase
   UNSAFE_componentWillReceiveProps(newProps) {
@@ -96,6 +104,7 @@ class Dataset extends React.Component {
           removed: result.catalog_record.removed,
           loaded: true,
         })
+        this.getAllVersions(result.catalog_record.dataset_version_set)
       })
       .catch(error => {
         console.log(error)
@@ -108,11 +117,17 @@ class Dataset extends React.Component {
     this.props.history.goBack()
   }
 
-  // modifies the message shown in notice bar in case the dataset is removed or deprecated
-  noticeBarText() {
+  async getAllVersions(data) {
+    const promises = [];
+
+    for (const k of data.keys()) {
+      const versionUrl = `/api/dataset/${data[k].identifier}`;
+      promises.push(axios.get(versionUrl))
+    }
+
+    const retval = await axios.all(promises) //will fetch all dataset versions
     let stateInfo = '';
     let urlText = '';
-    const data = this.state.dataset.dataset_version_set;
     let latestDate = '';
     const currentDate = new Date(this.state.dataset.date_created);
     let ID = '';
@@ -124,14 +139,12 @@ class Dataset extends React.Component {
       stateInfo = 'tombstone.deprecatedInfo'
     }
 
-    if (data.length > 1) { // / If there are other versions of the removed / deprecated dataset
-      linkToOtherVersion = 'tombstone.link'
-
-      latestDate = new Date(Math.max.apply(null, data // /Date of the latest existing version
-        .filter(version => !version.removed)
-        .map((version) =>
-          new Date(version.date_created)
-        )));
+    if (retval.length > 1) { // / If there are more than 1 version
+     latestDate = new Date(Math.max.apply(null, retval // /Date of the latest existing version
+      .filter(version => !version.data.catalog_record.removed && !version.data.catalog_record.deprecated)
+      .map((version) =>
+        new Date(version.data.catalog_record.date_created)
+      )));
 
       if (latestDate.getTime() > currentDate.getTime()) {
         urlText = 'tombstone.urlToNew'
@@ -139,22 +152,22 @@ class Dataset extends React.Component {
         urlText = 'tombstone.urlToOld'
       }
 
+      linkToOtherVersion = 'tombstone.link'
+
       for (const k of data.keys()) {
         if (new Date(data[k].date_created).getTime() === latestDate.getTime()) {
           ID = data[k].identifier;
+          break
         }
       }
-    }
 
-    return (
-      <div>
-        <Translate content={stateInfo} /><br />
-        <Translate content={urlText} />
-        <Link href={ID} target="_blank" rel="noopener noreferrer" content={'tombstone.link'}>
-          <Translate content={linkToOtherVersion} />
-        </Link>
-      </div>
-    )
+    this.setState({ versionInfo: {
+      stateInfo,
+      urlText,
+      ID,
+      linkToOtherVersion
+    } })
+    }
   }
 
 
@@ -166,14 +179,20 @@ class Dataset extends React.Component {
     // CASE 2: Business as usual
     return this.state.loaded ? (
       <div>
-        {(this.state.removed || this.state.deprecated) && (
-          <NoticeBar bg="error">
-            {this.noticeBarText()}
-          </NoticeBar>
-        )}
         <article className="container regular-row">
           <div className="row">
             <div className="col-12">
+              <div>
+              {(this.state.removed || this.state.deprecated) && (
+                <NoticeBar bg="error">
+                  <Translate content={this.state.versionInfo.stateInfo} /><br />
+                  <Translate content={this.state.versionInfo.urlText} />
+                  <Link href={this.state.versionInfo.ID} target="_blank" rel="noopener noreferrer" content={'tombstone.link'}>
+                    <Translate content={this.state.versionInfo.linkToOtherVersion} />
+                  </Link>
+                </NoticeBar>
+              )}
+              </div>
               <BackButton
                 exact
                 to="/datasets"
