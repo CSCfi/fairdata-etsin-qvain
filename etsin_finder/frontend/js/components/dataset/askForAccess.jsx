7f2e7070f95f61c0197a84d624151ccf270736e8
Kasper Keinänen <kasper.keinanen@csc.fi>
2019-12-18 13:01:07 +0200

    CSCFAIRMETA-4: Add redirect to REMS
    - The user will be redirected to REMS when applying for access as intended.
      Also disable the Apply for access button if user not logged in.
    - The onClick now waits for an answer from the backend and redirects the
      user to the right application.
    - Effects logged in user applying for rems permissions.

--- etsin_finder/frontend/js/components/dataset/askForAccess.jsx
+++ etsin_finder/frontend/js/components/dataset/askForAccess.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Translate from 'react-translate-component'
+import { inject, observer } from 'mobx-react'
 
 import axios from 'axios'
 import Button from '../general/button'
@@ -10,25 +11,37 @@ class AskForAccess extends Component {
   onClick = () => {
     axios
       .get(`/api/rems/${this.props.cr_id}`)
-      .then(console.log('OK'))
-      .catch(console.log('NOT OK'))
+      .then(res => {
+        console.log('OK')
+        console.log(res)
+        window.open(`https://vm1446.kaj.pouta.csc.fi/application/${res.data}`, '_blank')
+      })
+      .catch(err => {
+        console.log('NOT OK')
+        console.log(err)
+      })
   }
 
   render() {
     if (!access.restrictions.allowAskForPermit) {
       return null
     }
-    return (
-      // TODO: fill with real world items
+    const button = this.props.Stores.Auth.userLogged ? (
       <Button onClick={this.onClick} noMargin>
         <Translate content="dataset.access_permission" />
       </Button>
+    ) : (
+      <Button disabled noMargin>
+        <Translate content="dataset.access_permission" />
+      </Button>
     )
+    return button
   }
 }
 
 AskForAccess.propTypes = {
   cr_id: PropTypes.string.isRequired,
+  Stores: PropTypes.object.isRequired,
 }
 
-export default AskForAccess
+export default inject('Stores')(observer(AskForAccess))
