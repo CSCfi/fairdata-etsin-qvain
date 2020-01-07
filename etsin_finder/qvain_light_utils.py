c1b98a8bcb9dbad21be4561b7192610647ee6788
Mattias Levlin <11610718+MattiasLevlin@users.noreply.github.com>
2020-01-07 09:57:03 +0200

    CSCFAIRMETA-350: Error handling when no IDA projects (#400)
    * CSCFAIRMETA-350-no-ida-projects: Fix typo
    
    - Fixed a typo (fromat --> format)
    - Simple name fix
    - No side effects. This might have caused the previous issue where Etsin was left in a loading state.
    
    * CSCFAIRMETA-350-no-ida-projects: Error handling for case where user has no IDA projects
    
    - Error handling was not sufficient to handle cases where no IDA projects were found
    - Added separate error handling functionality for this case ('IdaError')
    - No side effects

--- etsin_finder/qvain_light_utils.py
+++ etsin_finder/qvain_light_utils.py
@@ -311,7 +311,7 @@ def check_if_data_in_user_IDA_project(data):
     try:
         user_ida_projects_ids = [project.split(":")[1] for project in user_ida_projects]
     except IndexError as e:
-        log.error('Index error while parsing user IDA projects:\n{0}'.fromat(e))
+        log.error('Index error while parsing user IDA projects:\n{0}'.format(e))
         return False
     if not user_ida_projects:
         log.warning('Could not get user IDA groups.')
