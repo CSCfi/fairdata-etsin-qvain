dd8f2f425ee018eee5404e19b3735e1cd86e74b1
Kasper Keinänen <kasper.keinanen@csc.fi>
2020-01-16 16:09:58 +0200

    CSCFAIRMETA-354: Update frontend access rights tests.
    - The previous implementation used GET method to take actual dataset from
      Metax. Now the part that was used is mocked by hard coding the data to
      a constant. This is done so that the test don't rely on the connection
      to the internal metax test environment.
    - Hard coded the part that was fetched every time.
    - If the data schema for access_rights change, then the hard coded constant
      should be updated.

--- etsin_finder/frontend/__tests__/access.test.jsx
+++ etsin_finder/frontend/__tests__/access.test.jsx
@@ -1,28 +1,91 @@
 import axios from 'axios'
 import Access from '../js/stores/view/access'
 import auth from '../js/stores/domain/auth'
+import { AccessTypeURLs } from '../js/components/qvain/utils/constants'
 
-const accessTypes = {
-  open: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/open',
-  login: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/login',
-  embargo: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/embargo',
-  permit: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/permit',
-  restricted: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/restricted',
+// Replaces getting from metax test:
+// axios.get('https://metax-test.csc.fi/rest/datasets/13').then(response => {
+//   accessRights = response.data.research_dataset.access_rights
+//   done()
+// })
+let accessRights = {
+  license: [
+    {
+      title: {
+        en: 'Apache Software License 2.0',
+        und: 'Apache Software License 2.0',
+      },
+      license: 'https://url.of.license.which.applies.here.org',
+      identifier: 'http://uri.suomi.fi/codelist/fairdata/license/code/Apache-2.0',
+      description: {
+        en: 'Free account of the rights',
+      },
+    },
+    {
+      title: {
+        en: 'Creative Commons Attribution-NonCommercial 2.0 Generic (CC BY-NC 2.0',
+        fi: 'Creative Commons Nimeä-EiKaupallinen 2.0 Yleinen (CC BY-NC 2.0)',
+        und: 'Creative Commons Nimeä-EiKaupallinen 2.0 Yleinen (CC BY-NC 2.0)',
+      },
+      license: 'https://creativecommons.org/licenses/by-nc/2.0/',
+      identifier: 'http://uri.suomi.fi/codelist/fairdata/license/code/CC-BY-NC-2.0',
+      description: {
+        en: 'Free account of the rights',
+      },
+    },
+    {
+      title: {
+        en: 'Other',
+        fi: 'Muu',
+        und: 'Muu',
+      },
+      identifier: 'http://uri.suomi.fi/codelist/fairdata/license/code/other',
+    },
+  ],
+  available: '2014-01-15',
+  access_url: {
+    title: {
+      en: 'A name given to the document',
+    },
+    identifier: 'https://access.url.com/landing',
+    description: {
+      en: 'Description of the link. For example to be used as hover text.',
+    },
+  },
+  access_type: {
+    in_scheme: 'http://uri.suomi.fi/codelist/fairdata/access_type',
+    definition: {
+      en: 'A statement or formal explanation of the meaning of a concept.',
+    },
+    identifier: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/open',
+    pref_label: {
+      en: 'Open',
+      fi: 'Avoin',
+      und: 'Avoin',
+    },
+  },
+  description: {
+    en: 'Free account of the rights',
+  },
+  restriction_grounds: [
+    {
+      in_scheme: 'http://uri.suomi.fi/codelist/fairdata/restriction_grounds',
+      identifier: 'http://uri.suomi.fi/codelist/fairdata/restriction_grounds/code/other',
+      pref_label: {
+        en: 'Restricted access due to other reasons',
+        fi: 'Saatavuutta rajoitettu muulla perusteella',
+        sv: 'Begränsad åtkomst av övriga skäl',
+        und: 'Saatavuutta rajoitettu muulla perusteella',
+      },
+    },
+  ],
 }
 
-let accessRights
-
 describe('Access Store', () => {
-  it('get data', done => {
-    axios.get('https://metax-test.csc.fi/rest/datasets/13').then(response => {
-      accessRights = response.data.research_dataset.access_rights
-      done()
-    })
-  })
   describe('Update for open access', () => {
     it('Should start update process', done => {
       const open = accessRights
-      open.access_type.identifier = accessTypes.open
+      open.access_type.identifier = AccessTypeURLs.OPEN
       Access.updateAccess(open)
       done()
     })
@@ -49,7 +112,7 @@ describe('Access Store', () => {
         const d = new Date()
         d.setFullYear(d.getFullYear() - 1)
         embargoed.available = d.toISOString()
-        embargoed.access_type.identifier = accessTypes.embargo
+        embargoed.access_type.identifier = AccessTypeURLs.EMBARGO
         Access.updateAccess(embargoed)
         done()
       })
@@ -75,7 +138,7 @@ describe('Access Store', () => {
         const d = new Date()
         d.setFullYear(d.getFullYear() + 1)
         embargoed.available = d.toISOString()
-        embargoed.access_type.identifier = accessTypes.embargo
+        embargoed.access_type.identifier = AccessTypeURLs.EMBARGO
         Access.updateAccess(embargoed)
         done()
       })
@@ -99,7 +162,7 @@ describe('Access Store', () => {
   describe('Update for restricted access', () => {
     it('Should start update process', done => {
       const restricted = accessRights
-      restricted.access_type.identifier = accessTypes.restricted
+      restricted.access_type.identifier = AccessTypeURLs.RESTRICTED
       Access.updateAccess(restricted)
       done()
     })
@@ -122,7 +185,7 @@ describe('Access Store', () => {
   describe('Update for permit access', () => {
     it('Should start update process', done => {
       const restricted_fairdata = accessRights
-      restricted_fairdata.access_type.identifier = accessTypes.permit
+      restricted_fairdata.access_type.identifier = AccessTypeURLs.PERMIT
       Access.updateAccess(restricted_fairdata)
       done()
     })
@@ -147,7 +210,7 @@ describe('Access Store', () => {
       it('Should start update process', done => {
         auth.userLogged = false
         const restricted_registration = accessRights
-        restricted_registration.access_type.identifier = accessTypes.login
+        restricted_registration.access_type.identifier = AccessTypeURLs.LOGIN
         Access.updateAccess(restricted_registration)
         done()
       })
@@ -171,7 +234,7 @@ describe('Access Store', () => {
       it('Should start update process', done => {
         auth.userLogged = true
         const restricted_registration = accessRights
-        restricted_registration.access_type.identifier = accessTypes.login
+        restricted_registration.access_type.identifier = AccessTypeURLs.LOGIN
         Access.updateAccess(restricted_registration)
         done()
       })
--- etsin_finder/frontend/js/components/qvain/utils/constants.js
+++ etsin_finder/frontend/js/components/qvain/utils/constants.js
@@ -1,30 +1,33 @@
 export const AccessTypeURLs = {
   EMBARGO: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/embargo',
-  OPEN: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/open'
+  OPEN: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/open',
+  LOGIN: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/login',
+  PERMIT: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/permit',
+  RESTRICTED: 'http://uri.suomi.fi/codelist/fairdata/access_type/code/restricted',
 }
 
 export const LicenseUrls = {
-  CCBY4: 'http://uri.suomi.fi/codelist/fairdata/license/code/CC-BY-4.0'
+  CCBY4: 'http://uri.suomi.fi/codelist/fairdata/license/code/CC-BY-4.0',
 }
 
 export const FileAPIURLs = {
   DIR_URL: '/api/files/directory/',
-  PROJECT_DIR_URL: '/api/files/project/'
+  PROJECT_DIR_URL: '/api/files/project/',
 }
 
 export const UseCategoryURLs = {
-  OUTCOME_MATERIAL: 'http://uri.suomi.fi/codelist/fairdata/use_category/code/outcome'
+  OUTCOME_MATERIAL: 'http://uri.suomi.fi/codelist/fairdata/use_category/code/outcome',
 }
 
 export const DataCatalogIdentifiers = {
   IDA: 'urn:nbn:fi:att:data-catalog-ida',
-  ATT: 'urn:nbn:fi:att:data-catalog-att'
+  ATT: 'urn:nbn:fi:att:data-catalog-att',
 }
 
 export const CumulativeStates = {
   NO: 0,
   YES: 1,
-  CLOSED: 2
+  CLOSED: 2,
 }
 
 export const EntityType = {
@@ -37,5 +40,5 @@ export const Role = {
   PUBLISHER: 'publisher',
   CURATOR: 'curator',
   RIGHTS_HOLDER: 'rights_holder',
-  CONTRIBUTOR: 'contributor'
+  CONTRIBUTOR: 'contributor',
 }
