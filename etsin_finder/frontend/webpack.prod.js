4539f169e705dca7a18d200e7155ae0f61be4f3c
Kasper Keinänen <kasper.keinanen@csc.fi>
2020-02-03 16:17:27 +0200

    CSCFAIRMETA-409: Update vulnerable packages + change UglifyJS to Terser.
    - Some packages had old versions that needed to be updated. UglifyJS,
      the javascript minification package for the production build had been
      discontinued, thus the need to change it to Terser.
    - Added the optimizations field to the webpack config containing the
      TerserPlugin part.
    - There should not be any side effects, since the Terser plugin should
      do the minification in a similar way as UglifyJS.

--- etsin_finder/frontend/webpack.prod.js
+++ etsin_finder/frontend/webpack.prod.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 const config = {
   entry: [path.join(__dirname, '/js/index.jsx')],
@@ -42,7 +42,24 @@ const config = {
       template: 'static/index.template.ejs',
       favicon: 'static/images/favicon.png',
     }),
-    new UglifyJSPlugin(),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        // Enable file caching. Default path to cache directory: node_modules/.cache/terser-webpack-plugin.
+        cache: true,
+        // Use multi-process parallel running to improve the build speed.
+        // Default number of concurrent runs: os.cpus().length - 1.
+        parallel: true,
+        terserOptions: {
+          // Support Internet Explorer 8.
+          ie8: true,
+          // Support Safari 10 with work around for Safari 10/11 bugs in loop scoping and await
+          safari10: true,
+        },
+      }),
+    ],
+  },
 }
 module.exports = config
