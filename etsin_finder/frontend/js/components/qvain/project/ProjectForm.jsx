a7245e9786446ab533fc495dbca280c6b1ab22dd
Roni Lindholm <roni.lindholm@csc.fi>
2020-07-28 11:30:53 +0300

    Implement project form component

---
+++ etsin_finder/frontend/js/components/qvain/project/ProjectForm.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Translate from 'react-translate-component'
+
+import { ProjectFunderType } from '../../../stores/view/qvain'
+import { LabelLarge, Input } from '../general/form'
+import Select from '../general/select'
+
+
+const ProjectForm = ({ onChange, formData }) => (
+  <>
+    <ProjectLabel
+      htmlFor="titleEn"
+      title="qvain.project.inputs.title.label"
+      description="qvain.project.inputs.title.description"
+    />
+    <Translate
+      component={Input}
+      value={formData.titleEn}
+      onChange={event => onChange(event.target.id, event.target.value)}
+      attributes={{ placeholder: 'qvain.project.inputs.titleEn.placeholder' }}
+      id="titleEn"
+    />
+    <Translate
+      component={Input}
+      value={formData.titleFi}
+      onChange={event => onChange(event.target.id, event.target.value)}
+      attributes={{ placeholder: 'qvain.project.inputs.titleFi.placeholder' }}
+      id="titleFi"
+    />
+    <ProjectLabel
+      htmlFor="identifier"
+      title="qvain.project.inputs.identifier.label"
+      description="qvain.project.inputs.identifier.description"
+    />
+    <Translate
+      component={Input}
+      value={formData.identifier}
+      onChange={event => onChange(event.target.id, event.target.value)}
+      attributes={{ placeholder: 'qvain.project.inputs.identifier.placeholder' }}
+      id="identifier"
+    />
+    <ProjectLabel
+      htmlFor="fundingIdentifier"
+      title="qvain.project.inputs.fundingIdentifier.label"
+      description="qvain.project.inputs.fundingIdentifier.description"
+    />
+    <Translate
+      component={Input}
+      value={formData.fundingIdentifier}
+      onChange={event => onChange(event.target.id, event.target.value)}
+      attributes={{ placeholder: 'qvain.project.inputs.fundingIdentifier.placeholder' }}
+      id="fundingIdentifier"
+    />
+    <ProjectLabel
+      htmlFor="funderType"
+      title="qvain.project.inputs.funderType.label"
+    />
+    <FunderTypeSelect onChange={onChange} value={formData.funderType} />
+  </>
+)
+
+
+ProjectForm.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  formData: PropTypes.object.isRequired,
+}
+
+const FunderTypeSelect = ({ value, onChange }) => (
+  <Select
+    name="funder-type"
+    getter={value}
+    setter={(newValue) => onChange('funderType', newValue) }
+    model={ProjectFunderType}
+    metaxIdentifier="funder_type"
+    placeholder="qvain.project.inputs.funderType.placeholder"
+    noOptionsMessage="qvain.project.inputs.funderType.noOptions"
+  />
+)
+
+FunderTypeSelect.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.object.isRequired,
+}
+
+const ProjectLabel = ({ htmlFor, title, description }) => (
+  <>
+    <LabelLarge htmlFor={htmlFor}>
+      <Translate content={title} />
+    </LabelLarge>
+    { description &&
+      <Translate component="p" content={description} /> }
+  </>
+)
+
+ProjectLabel.propTypes = {
+  htmlFor: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+}
+
+ProjectLabel.defaultProps = {
+  description: null,
+}
+
+export default ProjectForm
