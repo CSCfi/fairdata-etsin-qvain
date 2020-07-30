b0f47ae67799bf0aef4a30a002911669fe892212
Jori Niemi <3295718+tahme@users.noreply.github.com>
2020-07-30 09:37:54 +0300

    remove unused import

--- etsin_finder/frontend/js/components/qvain/datasets/dataset.jsx
+++ etsin_finder/frontend/js/components/qvain/datasets/dataset.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
 import PropTypes from 'prop-types'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import Translate from 'react-translate-component'
 import { tint } from 'polished'
 
