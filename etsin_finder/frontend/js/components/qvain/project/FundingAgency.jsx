57a5a8299e04c46da5524afd09110d7226671ac0
Roni Lindholm <roni.lindholm@csc.fi>
2020-08-27 13:26:48 +0300

    Refactor funding agency form to use select for contributor type id

--- etsin_finder/frontend/js/components/qvain/project/FundingAgency.jsx
+++ etsin_finder/frontend/js/components/qvain/project/FundingAgency.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Translate from 'react-translate-component'
 import { inject, observer } from 'mobx-react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import ReactSelect from 'react-select'
+import axios from 'axios'
 
 import Card from '../general/card'
 import OrganizationSelect from '../general/organizationSelect'
 import Button from '../../general/button'
 import Label from '../general/label'
-import { ErrorMessages, validate, isEmptyObject, organizationToSelectValue, Expand } from './utils'
+import { ErrorMessages, organizationSelectValueToSchema, validateSync, validate,
+  isEmptyObject, organizationToSelectValue, Expand, referenceDataToOptions } from './utils'
 import { LabelLarge, Input } from '../general/form'
-import { fundingAgencySchema } from '../utils/formValidation'
-import { FundingAgency, ContributorType } from '../../../stores/view/qvain'
+import { fundingAgencySchema, organizationSelectSchema } from '../utils/formValidation'
+import { FundingAgency, ContributorType, Organization } from '../../../stores/view/qvain'
+import { METAX_FAIRDATA_ROOT_URL } from '../../../utils/constants'
 
 
 const FundingAgencyForm = props => {
@@ -51,7 +55,7 @@ const FundingAgencyForm = props => {
     const { formData } = props.value
     const contributorTypes = formData.contributorTypes
       .filter(type => type.id !== id)
-    props.onChange({ ...formData, contributorTypes })
+    props.onChange({ ...formData, contributorTypeForm: { errors: {} }, contributorTypes })
   }
 
   /**
@@ -63,8 +67,14 @@ const FundingAgencyForm = props => {
     const agencyToEdit = addedFundingAgencies.find(agency => agency.id === id)
     if (!agencyToEdit) return
     const { organization, contributorTypes } = agencyToEdit
-    const department = await organizationToSelectValue(organization.department, lang, organization.organization ? organization.organization.identifier : null, lang)
-    const subDepartment = await organizationToSelectValue(organization.subDepartment, lang, organization.department ? organization.department.identifier : null, lang)
+    const department = await organizationToSelectValue(
+      organization.department, lang,
+      organization.organization ? organization.organization.identifier : null, lang
+    )
+    const subDepartment = await organizationToSelectValue(
+      organization.subDepartment, lang,
+      organization.department ? organization.department.identifier : null, lang
+    )
     props.onChange({
       id,
       organization: {
@@ -77,15 +87,38 @@ const FundingAgencyForm = props => {
     })
   }
 
+  /**
+   *
+   */
+  const validateAll = organizations => {
+    let valid = true
+    const { formData } = props.value
+    for (const [key, value] of Object.entries(organizations)) {
+      if (value) {
+        const errors = validateSync(organizationSelectSchema, value)
+        formData.organization[key].errors = errors
+        if (!isEmptyObject(errors)) valid = false
+      }
+    }
+    props.onChange(formData)
+    return valid
+  }
 
   /**
    * Construct a funding agency object and pass it to the parent on add handler.
-   * This expects teh child objects (organization and funder types) to be
-   * validated and valid.
+   * This expects added contributor types to be valid. Only the organization
+   * is validated here.
    */
   const onAddAgency = () => {
-    const { id, organization, contributorTypes } = props.value.formData
-    const agency = FundingAgency(id, organization, contributorTypes)
+    const { formData } = props.value
+    const { id, contributorTypes } = formData
+    const organization = organizationSelectValueToSchema(formData.organization.organization)
+    const department = organizationSelectValueToSchema(formData.organization.department)
+    const subDepartment = organizationSelectValueToSchema(formData.organization.subDepartment)
+    if (!validateAll({ organization, department, subDepartment })) return
+
+    const organizationObject = Organization(null, organization, department, subDepartment)
+    const agency = FundingAgency(id, organizationObject, contributorTypes)
     props.onAdd(agency)
   }
 
@@ -171,24 +204,64 @@ AddedAgencies.defaultProps = {
   agencies: [],
 }
 
-const ContributorTypeForm = ({ onChange, onAdd, onRemove, formData, contributorTypes, readonly }) => {
-  const onBlur = async () => {
+class ContributorTypeFormComponent extends Component {
+  static propTypes = {
+    Stores: PropTypes.object.isRequired,
+    onChange: PropTypes.func.isRequired,
+    onAdd: PropTypes.func.isRequired,
+    onRemove: PropTypes.func.isRequired,
+    formData: PropTypes.object.isRequired,
+    contributorTypes: PropTypes.array.isRequired,
+    readonly: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    readonly: false,
+  }
+
+  state = {
+    options: {}
+  }
+
+  componentDidMount() {
+    this.fetchOptions()
+  }
+
+  fetchOptions = async () => {
+    const response = await axios.get(`${METAX_FAIRDATA_ROOT_URL}/es/reference_data/contributor_type/_search?pretty=true&size=100`)
+    if (response.status !== 200) return
+    const options = referenceDataToOptions(response.data.hits.hits)
+    console.log(options)
+    this.setState({ options })
+  }
+
+  onBlur = async () => {
+    const { formData, onChange } = this.props
     const errors = await validate(fundingAgencySchema, { ...formData })
     onChange({ ...formData, errors })
   }
 
-  const onFieldChange = event => {
+  onFieldChange = event => {
+    const { formData, onChange } = this.props
     const { name, value } = event.target
     onChange({ ...formData, [name]: value })
   }
 
-  const onAddType = async () => {
-    const errors = await validate(fundingAgencySchema, { ...formData })
+  onSelectChange = value => {
+    const { formData, onChange } = this.props
+    onChange({ ...formData, identifier: value })
+  }
+
+  onAddType = async () => {
+    const { formData, onChange, onAdd } = this.props
+    const errors = await validate(fundingAgencySchema, { ...formData, identifier: formData.identifier.value })
     if (!isEmptyObject(errors)) onChange({ ...formData, errors })
     else onAdd()
   }
 
-  const onEditType = id => {
+  onEditType = id => {
+    const { contributorTypes, onChange } = this.props
+
     const contributorTypeToEdit = contributorTypes
       .find(contributorType => contributorType.id === id)
     if (!contributorTypeToEdit) return
@@ -205,107 +278,118 @@ const ContributorTypeForm = ({ onChange, onAdd, onRemove, formData, contributorT
     })
   }
 
-  return (
-    <Card>
-      <Translate component="p" content="qvain.project.inputs.fundingAgency.contributorType.description" />
-      <AddedContributorTypes
-        contributorTypes={contributorTypes}
-        onEdit={onEditType}
-        onRemove={onRemove}
-      />
-      <LabelLarge htmlFor="identifier">
-        <Translate content="qvain.project.inputs.fundingAgency.contributorType.identifier.label" />
-      </LabelLarge>
-      <Translate
-        component={Input}
-        value={formData.identifier || ''}
-        onChange={onFieldChange}
-        onBlur={onBlur}
-        attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.identifier.placeholder' }}
-        disabled={readonly}
-        name="identifier"
-        id="identifier"
-      />
-      <ErrorMessages errors={formData.errors.identifier} />
-      <LabelLarge htmlFor="titleEn">
-        <Translate content="qvain.project.inputs.fundingAgency.contributorType.label.label" />
-      </LabelLarge>
-      <Translate component="p" content="qvain.project.inputs.fundingAgency.contributorType.label.description" />
-      <Translate
-        component={Input}
-        value={formData.labelEn || ''}
-        onChange={onFieldChange}
-        onBlur={onBlur}
-        attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.label.placeholderEn' }}
-        disabled={readonly}
-        name="labelEn"
-        id="labelEn"
-      />
-      <Translate
-        component={Input}
-        value={formData.labelFi || ''}
-        onChange={onFieldChange}
-        onBlur={onBlur}
-        attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.label.placeholderFi' }}
-        disabled={readonly}
-        name="labelFi"
-        id="labelFi"
-      />
-      <LabelLarge htmlFor="definitionEn">
-        <Translate content="qvain.project.inputs.fundingAgency.contributorType.definition.label" />
-      </LabelLarge>
-      <Translate component="p" content="qvain.project.inputs.fundingAgency.contributorType.definition.description" />
-      <Translate
-        component={Input}
-        value={formData.definitionEn || ''}
-        onChange={onFieldChange}
-        onBlur={onBlur}
-        attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.definition.placeholderEn' }}
-        disabled={readonly}
-        name="definitionEn"
-        id="definitionEn"
-      />
-      <Translate
-        component={Input}
-        value={formData.definitionFi || ''}
-        onChange={onFieldChange}
-        onBlur={onBlur}
-        attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.definition.placeholderFi' }}
-        disabled={readonly}
-        name="definitionFi"
-        id="definitionFi"
-      />
-      <LabelLarge htmlFor="inScheme">
-        <Translate content="qvain.project.inputs.fundingAgency.contributorType.inScheme.label" />
-      </LabelLarge>
-      <Translate component="p" content="qvain.project.inputs.fundingAgency.contributorType.inScheme.description" />
-      <Translate
-        component={Input}
-        value={formData.inScheme || ''}
-        onChange={onFieldChange}
-        onBlur={onBlur}
-        attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.inScheme.placeholder' }}
-        disabled={readonly}
-        name="inScheme"
-        id="inScheme"
-      />
-      <AddAgencyContainer>
-        <Button onClick={onAddType}>
-          <Translate content={formData.id
-            ? 'qvain.project.inputs.fundingAgency.contributorType.editButton'
-            : 'qvain.project.inputs.fundingAgency.contributorType.addButton'}
-          />
-        </Button>
-      </AddAgencyContainer>
-    </Card>
-  )
+  render() {
+    const { contributorTypes, onRemove, readonly, formData } = this.props
+    const { lang } = this.props.Stores.Locale
+    const { options } = this.state
+
+    return (
+      <Card>
+        <Translate component="p" content="qvain.project.inputs.fundingAgency.contributorType.description" />
+        <AddedContributorTypes
+          contributorTypes={contributorTypes}
+          onEdit={this.onEditType}
+          onRemove={onRemove}
+          lang={lang}
+        />
+        <LabelLarge htmlFor="identifier">
+          <Translate content="qvain.project.inputs.fundingAgency.contributorType.identifier.label" />
+        </LabelLarge>
+        <Translate
+          component={ReactSelect}
+          name="identifier"
+          inputId="identifier"
+          isDisabled={readonly}
+          onChange={this.onSelectChange}
+          value={formData.identifier}
+          className="basic-single"
+          classNamePrefix="select"
+          options={options[lang]}
+          attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.identifier.placeholder' }}
+        />
+        <ErrorMessages errors={formData.errors.identifier} />
+        <LabelLarge htmlFor="titleEn">
+          <Translate content="qvain.project.inputs.fundingAgency.contributorType.label.label" />
+        </LabelLarge>
+        <Translate component="p" content="qvain.project.inputs.fundingAgency.contributorType.label.description" />
+        <Translate
+          component={Input}
+          value={formData.labelEn || ''}
+          onChange={this.onFieldChange}
+          onBlur={this.onBlur}
+          attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.label.placeholderEn' }}
+          disabled={readonly}
+          name="labelEn"
+          id="labelEn"
+        />
+        <Translate
+          component={Input}
+          value={formData.labelFi || ''}
+          onChange={this.onFieldChange}
+          onBlur={this.onBlur}
+          attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.label.placeholderFi' }}
+          disabled={readonly}
+          name="labelFi"
+          id="labelFi"
+        />
+        <LabelLarge htmlFor="definitionEn">
+          <Translate content="qvain.project.inputs.fundingAgency.contributorType.definition.label" />
+        </LabelLarge>
+        <Translate component="p" content="qvain.project.inputs.fundingAgency.contributorType.definition.description" />
+        <Translate
+          component={Input}
+          value={formData.definitionEn || ''}
+          onChange={this.onFieldChange}
+          onBlur={this.onBlur}
+          attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.definition.placeholderEn' }}
+          disabled={readonly}
+          name="definitionEn"
+          id="definitionEn"
+        />
+        <Translate
+          component={Input}
+          value={formData.definitionFi || ''}
+          onChange={this.onFieldChange}
+          onBlur={this.onBlur}
+          attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.definition.placeholderFi' }}
+          disabled={readonly}
+          name="definitionFi"
+          id="definitionFi"
+        />
+        <LabelLarge htmlFor="inScheme">
+          <Translate content="qvain.project.inputs.fundingAgency.contributorType.inScheme.label" />
+        </LabelLarge>
+        <Translate component="p" content="qvain.project.inputs.fundingAgency.contributorType.inScheme.description" />
+        <Translate
+          component={Input}
+          value={formData.inScheme || ''}
+          onChange={this.onFieldChange}
+          onBlur={this.onBlur}
+          attributes={{ placeholder: 'qvain.project.inputs.fundingAgency.contributorType.inScheme.placeholder' }}
+          disabled={readonly}
+          name="inScheme"
+          id="inScheme"
+        />
+        <AddAgencyContainer>
+          <Button onClick={this.onAddType}>
+            <Translate content={formData.id
+              ? 'qvain.project.inputs.fundingAgency.contributorType.editButton'
+              : 'qvain.project.inputs.fundingAgency.contributorType.addButton'}
+            />
+          </Button>
+        </AddAgencyContainer>
+      </Card>
+    )
+  }
 }
 
-const AddedContributorTypes = ({ contributorTypes = [], onRemove, onEdit }) => (
+const ContributorTypeForm = inject('Stores')(observer(ContributorTypeFormComponent))
+
+const AddedContributorTypes = ({ contributorTypes = [], onRemove, onEdit, lang }) => (
   <div>
     { contributorTypes.map(contributorType => (
       <AgencyLabel color="#007fad" margin="0 0.5em 0.5em 0" key={contributorType.id}>
-        <PaddedWord onClick={() => onEdit(contributorType.id)}>{contributorType.identifier }</PaddedWord>
+        <PaddedWord onClick={() => onEdit(contributorType.id)}>{contributorType.identifier.name[lang] }</PaddedWord>
         <FontAwesomeIcon
           onClick={() => onRemove(contributorType.id)}
           icon={faTimes}
@@ -321,23 +405,11 @@ AddedContributorTypes.propTypes = {
   contributorTypes: PropTypes.array,
   onRemove: PropTypes.func.isRequired,
   onEdit: PropTypes.func.isRequired,
+  lang: PropTypes.string.isRequired,
 }
 
 AddedContributorTypes.defaultProps = {
   contributorTypes: [],
-}
-
-ContributorTypeForm.propTypes = {
-  onChange: PropTypes.func.isRequired,
-  onAdd: PropTypes.func.isRequired,
-  onRemove: PropTypes.func.isRequired,
-  formData: PropTypes.object.isRequired,
-  contributorTypes: PropTypes.array.isRequired,
-  readonly: PropTypes.bool,
-}
-
-ContributorTypeForm.defaultProps = {
-  readonly: false,
 }
 
 const AddAgencyContainer = styled.div`
