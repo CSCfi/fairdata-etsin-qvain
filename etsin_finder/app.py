90535e2a5235788273551388b7b2c8c19683af15
Ismo Torvinen <ismo.torvinen@gmail.com>
2020-09-18 13:01:27 +0300

    undo local dev settings from app.py

--- etsin_finder/app.py
+++ etsin_finder/app.py
@@ -24,8 +24,8 @@ def create_app():
     app = Flask(__name__, template_folder="./frontend/build")
 
     app.config.update(get_app_config(is_testing))
-#    if not app.testing and not executing_travis():
-#        _setup_app_logging(app)
+    if not app.testing and not executing_travis():
+        _setup_app_logging(app)
     if not executing_travis():
         app.config.update({'SAML_PATH': '/home/etsin-user'})
     app.mail = Mail(app)
