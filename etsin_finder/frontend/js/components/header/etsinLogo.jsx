c420b7ff31a4d39e7291f6c3dd2f82c0ff123754
Jori Niemi <3295718+tahme@users.noreply.github.com>
2020-09-22 15:52:26 +0300

    CSCFAIRMETA-718: Qvain navbar (#549)
    * When running Qvain in separate site, show different navbar content
    * Make header components more general so Etsin and Qvain can share theme
    * Add Qvain logo
    * Resize Qvain and Etsin logo images to be same size
    * Fix logout not working properly on narrow screen

---
+++ etsin_finder/frontend/js/components/header/etsinLogo.jsx
@@ -0,0 +1,20 @@
+{
+  /**
+   * This file is part of the Etsin service
+   *
+   * Copyright 2017-2018 Ministry of Education and Culture, Finland
+   *
+   *
+   * @author    CSC - IT Center for Science Ltd., Espoo Finland <servicedesk@csc.fi>
+   * @license   MIT
+   */
+}
+
+import React from 'react'
+
+import Logo from '../general/header/logo'
+import EtsinLogoImage from '../../../static/images/Etsin_300px.png'
+
+const EtsinLogo = () => <Logo image={EtsinLogoImage} alt="Fairdata Etsin logo" />
+
+export default EtsinLogo
