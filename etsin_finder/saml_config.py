45d8bac2392f1bedac75926dbf2c742e3eb9d8a8
Mattias Levlin <mattias.levlin@gmail.com>
2020-09-24 14:28:37 +0300

    New file: saml_config.py, for retrieving saml data to backend

---
+++ etsin_finder/saml_config.py
@@ -0,0 +1,33 @@
+# This file is part of the Etsin service
+#
+# Copyright 2017-2020 Ministry of Education and Culture, Finland
+#
+# :author: CSC - IT Center for Science Ltd., Espoo Finland <servicedesk@csc.fi>
+# :license: MIT
+
+"""Get saml configurations for authentication purposes"""
+
+import json
+from etsin_finder.log import log
+
+# from etsin_finder.utils import executing_travis
+
+def get_saml_config():
+    """Get saml config.
+
+    Returns:
+        saml_config
+
+    """
+    with open('/home/etsin-user/settings.json') as saml_json_file:
+        return json.load(saml_json_file)
+
+def get_sso_key():
+    """Get SSO key from saml config.
+
+    Returns:
+        saml_config
+
+    """
+    data = get_saml_config()
+    return data.get('sp').get('privateKey')
