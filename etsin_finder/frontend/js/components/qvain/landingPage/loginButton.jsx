4538100ab64788680bf0cc2f49c51ce1f8daade8
Mattias Levlin <mattias.levlin@gmail.com>
2020-10-02 09:53:54 +0300

    Fix

--- etsin_finder/frontend/js/components/qvain/landingPage/loginButton.jsx
+++ etsin_finder/frontend/js/components/qvain/landingPage/loginButton.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import LoginButton from '../../general/navigation/loginButton'
 
-const LargeLoginButton = () =>
-  (<LoginButton
-      fontSize="1.5rem"
-      margin="0"
-      loginThroughService="qvain"
-  />)
+const LargeLoginButton = () => (
+  <LoginButton
+    fontSize="1.5rem"
+    margin="0"
+    loginThroughService="qvain"
+  />
+)
 
 export default LargeLoginButton
