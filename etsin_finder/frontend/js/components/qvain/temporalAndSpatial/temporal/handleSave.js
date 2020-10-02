33061c90935070dddbe9a769b60497584dcc2ba3
Ismo Torvinen <ismo.torvinen@gmail.com>
2020-10-02 14:37:11 +0300

    fixed temporal clears values on add

--- etsin_finder/frontend/js/components/qvain/temporalAndSpatial/temporal/handleSave.js
+++ etsin_finder/frontend/js/components/qvain/temporalAndSpatial/temporal/handleSave.js
@@ -1,7 +1,7 @@
 import { temporalDateSchema } from '../../utils/formValidation'
 
 export default async (Store, Field) => {
-  const { inEdit, reset, setValidationError, addTemporal } = Field
+  const { inEdit, create, setValidationError, addTemporal } = Field
   try {
     await temporalDateSchema.validate(inEdit)
   } catch (e) {
@@ -9,5 +9,5 @@ export default async (Store, Field) => {
     return
   }
   addTemporal()
-  reset()
+  create()
 }
