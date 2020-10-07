f37a9aac8535935f23c972f81554141be91e16e6
Mattias Levlin <mattias.levlin@gmail.com>
2020-10-07 18:45:56 +0300

    Cookie notif from SSO

--- etsin_finder/frontend/js/layout/cookiesNotification.jsx
+++ etsin_finder/frontend/js/layout/cookiesNotification.jsx
@@ -8,16 +8,58 @@ import Button from '../components/general/button'
 
 class CookiesNotification extends Component {
   state = {
-    visible: !localStorage.getItem('cookiesAccepted'),
+    displayCookieNotification: true
   }
 
   componentDidMount() {
     counterpart.onLocaleChange(this.onLanguageChange)
+    if (Boolean(this.fdSSOGetCookie('fd_sso_notification_shown'))) {
+      this.setState({
+          displayCookieNotification: false
+        }
+      )
+    }
   }
 
   componentWillUnmount() {
     counterpart.offLocaleChange(this.onLanguageChange)
   }
+
+  fdSSOGetDomainName() {
+    let hostname = window.location.hostname;
+    let domain = hostname.substring(hostname.indexOf(".") + 1); // Check indexOf ".f" in local_dev
+    return domain
+}
+
+ fdSSOGetPrefixedCookieName(name) {
+    let domain = this.fdSSOGetDomainName();
+    domain = domain.replace(/[^a-zA-Z0-9]/g, "_")
+    return domain + "_" + name
+}
+
+ fdSSOGetCookie(name) {
+    name = this.fdSSOGetPrefixedCookieName(name)
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+ fdSSOSetCookie(name, value) {
+    name = this.fdSSOGetPrefixedCookieName(name);
+    let expiryDate = new Date();
+    expiryDate.setTime(expiryDate.getTime() + (7*24*60*60*1000));
+    let expires = "; expires=" + expiryDate.toUTCString();
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/" + "; domain=." + this.fdSSOGetDomainName();
+}
+
+ fdSSODismissNotification() {
+   this.fdSSOSetCookie("fd_sso_notification_shown", true)
+}
 
   onLanguageChange = () => {
     this.setState({
@@ -31,21 +73,16 @@ class CookiesNotification extends Component {
       : 'https://www.fairdata.fi/sopimukset/'
   }
 
-  handleAcceptCookies = () => {
-    localStorage.setItem('cookiesAccepted', true)
-    this.setState({ visible: false })
-  }
-
   render() {
     return (
-      <Notification visible={this.state.visible}>
+      <Notification visible={this.state.displayCookieNotification}>
         <div className="container row no-gutters">
           <div className="col-12 col-md-9">
             <Translate component="p" content="general.cookies.infoText" />
             <a href={this.getPrivacyUrl()} target="_blank" rel="noopener noreferrer"><Translate content="general.cookies.link" /></a>
           </div>
           <Actions className="col-12 col-md-3">
-            <Button onClick={this.handleAcceptCookies}>
+            <Button onClick={this.fdSSODismissNotification()}>
               <Translate content="general.cookies.accept" />
             </Button>
           </Actions>
