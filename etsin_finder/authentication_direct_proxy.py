781c5e58fc90c8a9d3aacc7f82eb1fa695936489
Mattias Levlin <11610718+MattiasLevlin@users.noreply.github.com>
2020-10-20 12:45:30 +0300

    CSCFAIRADM-407: [FIX] Support old login (#588)
    * Remove redundant, unused functionality
    
    * Simplify request
    
    * Fix, no separate validation for local

--- etsin_finder/authentication_direct_proxy.py
+++ etsin_finder/authentication_direct_proxy.py
@@ -64,19 +64,9 @@ def prepare_flask_request_for_saml(request, service):
     # If server is behind proxys or balancers use the HTTP_X_FORWARDED fields
     url_data = urlparse(request.url)
 
-    # If in local development environment this will redirect the legacy SAML login right.
-    if request.host == 'localhost':
-        http_host = '30.30.30.30'
-    # Non-local login through Etsin SSO
-    elif service == 'ETSIN':
-        http_host = get_app_config(app.testing).get('SERVER_ETSIN_DOMAIN_NAME')
-    # Non-local login through Qvain SSO
-    elif service == 'QVAIN':
-        http_host = get_app_config(app.testing).get('SERVER_QVAIN_DOMAIN_NAME')
-
     return {
         'https': 'on' if request.scheme == 'https' else 'off',
-        'http_host': http_host,
+        'http_host': get_app_config(app.testing).get('SERVER_ETSIN_DOMAIN_NAME'),
         'server_port': url_data.port,
         'script_name': request.path,
         'get_data': request.args.copy(),
