e035d8dc57c6eff9d0fd68ce86e0f120024d2bf9
Jori Niemi <3295718+tahme@users.noreply.github.com>
2021-02-09 14:01:39 +0200

    CSCFAIRMETA-948: Don't open top-level directories automatically

--- etsin_finder/frontend/js/components/dataset/data/idaResourcesV2/fileTree.jsx
+++ etsin_finder/frontend/js/components/dataset/data/idaResourcesV2/fileTree.jsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react'
-import { autorun } from 'mobx'
 import PropTypes from 'prop-types'
 import { observer } from 'mobx-react'
 
@@ -12,18 +10,6 @@ export function FileTree(props) {
   const { downloadApiV2 } = props.Stores.Env
   const { View } = Files
   const { allowDownload } = props
-
-  // Open top level directory
-  useEffect(
-    () =>
-      autorun(() => {
-        if (Files.root) {
-          Files.root.directories.forEach(dir => View.open(dir))
-        }
-      }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  )
 
   const { renderTree } = useRenderTree(
     {
--- etsin_finder/frontend/js/components/qvain/fields/files/ida/addItemsTree.jsx
+++ etsin_finder/frontend/js/components/qvain/fields/files/ida/addItemsTree.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { observer } from 'mobx-react'
-import { autorun } from 'mobx'
 import Translate from 'react-translate-component'
 
 import { useRenderTree } from '../../../../general/files/tree'
@@ -18,18 +17,6 @@ const AddItemsTree = () => {
     Qvain: { Files },
   } = useStores()
   const { AddItemsView } = Files
-
-  // Open top level directory
-  useEffect(
-    () =>
-      autorun(() => {
-        if (Files.root) {
-          Files.root.directories.forEach((dir) => AddItemsView.open(dir))
-        }
-      }),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  )
 
   const { renderTree } = useRenderTree({
     Files,
