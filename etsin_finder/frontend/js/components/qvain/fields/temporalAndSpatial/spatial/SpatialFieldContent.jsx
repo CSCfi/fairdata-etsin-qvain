262b13b115a8fb77cc5f34d4e0ce3c7559e5a673
Jori Niemi <3295718+tahme@users.noreply.github.com>
2021-02-19 15:59:43 +0200

    lint

--- etsin_finder/frontend/js/components/qvain/fields/temporalAndSpatial/spatial/SpatialFieldContent.jsx
+++ etsin_finder/frontend/js/components/qvain/fields/temporalAndSpatial/spatial/SpatialFieldContent.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { observer } from 'mobx-react'
 import FieldList from '../../../general/section/fieldList'
 import FieldListAdd from '../../../general/section/fieldListAdd'
