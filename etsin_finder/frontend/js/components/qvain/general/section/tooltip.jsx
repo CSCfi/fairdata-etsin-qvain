490335dc4af3e67016db13ceba4d6dcd0a5b2823
Ismo Torvinen <ismo.torvinen@gmail.com>
2021-03-10 12:17:00 +0200

    some tooltip refactor, tests for brief expand and tooltip, ComponentTestHarness

--- etsin_finder/frontend/js/components/qvain/general/section/tooltip.jsx
+++ etsin_finder/frontend/js/components/qvain/general/section/tooltip.jsx
@@ -1,6 +1,19 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import 'chai/register-expect'
+
+const setEventListeners = (updatePosition, handleClickOutside) => {
+  window.addEventListener('scroll', updatePosition)
+  window.addEventListener('resize', updatePosition)
+  document.addEventListener('mousedown', handleClickOutside)
+}
+
+const clearEventListeners = (updatePosition, handleClickOutside) => {
+  window.removeEventListener('scroll', updatePosition)
+  window.removeEventListener('resize', updatePosition)
+  document.removeEventListener('mousedown', handleClickOutside)
+}
 
 const Tooltip = ({ isOpen, close, align, text, children }) => {
   const [currentAlign, setCurrentAlign] = useState(align)
@@ -31,96 +44,39 @@ const Tooltip = ({ isOpen, close, align, text, children }) => {
     })
   }, [align])
 
-  useEffect(() => {
-    setCurrentAlign(align)
-    updatePosition()
-
-    const handleClickOutside = event => {
+  const handleClickOutside = useCallback(
+    event => {
       if (wrapperTooltipCardRef.current) {
         // If the tooltip card is clicked, tooltip should not be closed. Otherwise, close.
         if (!wrapperTooltipCardRef.current.contains(event.target)) {
           close()
         }
       }
-    }
+    },
+    [close]
+  )
 
-    window.addEventListener('scroll', updatePosition)
-    window.addEventListener('resize', updatePosition)
-    document.addEventListener('mousedown', handleClickOutside)
+  useEffect(() => {
+    setCurrentAlign(align)
+    updatePosition()
+    setEventListeners(updatePosition, handleClickOutside)
+
     return () => {
-      window.removeEventListener('scroll', updatePosition)
-      window.removeEventListener('resize', updatePosition)
-      document.removeEventListener('mousedown', handleClickOutside)
+      clearEventListeners(updatePosition, handleClickOutside)
     }
-  }, [align, close, updatePosition])
+  }, [align, handleClickOutside, updatePosition])
 
-  let tooltip
-  switch (currentAlign) {
-    case 'Right':
-      tooltip = (
-        <>
-          <span ref={wrapperTooltipButtonRef}>{children}</span>
-          <Wrapper ref={wrapperTooltipCardRef}>
-            <TooltipRight>
-              <TooltipArrowRight />
-              <TooltipText>{text}</TooltipText>
-            </TooltipRight>
-          </Wrapper>
-        </>
-      )
-      break
-    case 'Left':
-      tooltip = (
-        <>
-          <span ref={wrapperTooltipButtonRef}>{children}</span>
-          <Wrapper ref={wrapperTooltipCardRef}>
-            <TooltipLeft>
-              <TooltipText>{text}</TooltipText>
-              <TooltipArrowLeft />
-            </TooltipLeft>
-          </Wrapper>
-        </>
-      )
-      break
-    case 'Down':
-      tooltip = (
-        <>
-          <span ref={wrapperTooltipButtonRef}>{children}</span>
-          <Wrapper ref={wrapperTooltipCardRef}>
-            <TooltipDown>
-              <TooltipArrowDown />
-              <TooltipText>{text}</TooltipText>
-            </TooltipDown>
-          </Wrapper>
-        </>
-      )
-      break
-    case 'Up':
-      tooltip = (
-        <>
-          <span ref={wrapperTooltipButtonRef}>{children}</span>
-          <Wrapper ref={wrapperTooltipCardRef}>
-            <TooltipUp>
-              <TooltipText>{text}</TooltipText>
-              <TooltipArrowUp />
-            </TooltipUp>
-          </Wrapper>
-        </>
-      )
-      break
-    default:
-      tooltip = (
-        <>
-          <span ref={wrapperTooltipButtonRef}>{children}</span>
-          <Wrapper ref={wrapperTooltipCardRef}>
-            <TooltipDown>
-              <TooltipText>{text}</TooltipText>
-              <TooltipArrowDown />
-            </TooltipDown>
-          </Wrapper>
-        </>
-      )
-  }
+  const tooltip = (
+    <AlignedTooltip
+      buttonRef={wrapperTooltipButtonRef}
+      cardRef={wrapperTooltipCardRef}
+      alignment={currentAlign}
+      text={text}
+    >
+      {children}
+    </AlignedTooltip>
+  )
+
   return isOpen ? tooltip : children
 }
 
@@ -133,6 +89,56 @@ Tooltip.propTypes = {
 }
 
 export default Tooltip
+
+export const AlignedTooltip = ({ buttonRef, cardRef, text, children, alignment = 'Down' }) => {
+  const mappedTooltipComponents = {
+    Right: (
+      <TooltipRight>
+        <TooltipArrowRight />
+        <TooltipText>{text}</TooltipText>
+      </TooltipRight>
+    ),
+    Left: (
+      <TooltipLeft>
+        <TooltipText>{text}</TooltipText>
+        <TooltipArrowLeft />
+      </TooltipLeft>
+    ),
+    Up: (
+      <TooltipUp>
+        <TooltipArrowUp />
+        <TooltipText>{text}</TooltipText>
+      </TooltipUp>
+    ),
+    Down: (
+      <TooltipDown>
+        <TooltipText>{text}</TooltipText>
+        <TooltipArrowDown />
+      </TooltipDown>
+    ),
+  }
+
+  const alignedTooltipComponent = mappedTooltipComponents[alignment]
+
+  return (
+    <>
+      <span ref={buttonRef}>{children}</span>
+      <Wrapper ref={cardRef}>{alignedTooltipComponent}</Wrapper>
+    </>
+  )
+}
+
+AlignedTooltip.propTypes = {
+  buttonRef: PropTypes.object.isRequired,
+  cardRef: PropTypes.object.isRequired,
+  alignment: PropTypes.string,
+  text: PropTypes.element.isRequired,
+  children: PropTypes.element.isRequired,
+}
+
+AlignedTooltip.defaultProps = {
+  alignment: 'Down',
+}
 
 const Wrapper = styled.span`
   position: relative;
@@ -152,28 +158,28 @@ const TooltipStyle = styled.div`
   position: absolute;
 `
 
-const TooltipLeft = styled(TooltipStyle)`
+export const TooltipLeft = styled(TooltipStyle)`
   display: flex;
   margin-left: 5px;
   right: 30px;
   top: -8px;
 `
 
-const TooltipRight = styled(TooltipStyle)`
+export const TooltipRight = styled(TooltipStyle)`
   display: flex;
   margin-left: -5px;
   left: 0px;
   top: -8px;
 `
 
-const TooltipDown = styled(TooltipStyle)`
+export const TooltipDown = styled(TooltipStyle)`
   display: inline-block;
   margin-top: -5px;
   left: -37px;
   top: 30px;
 `
 
-const TooltipUp = styled(TooltipStyle)`
+export const TooltipUp = styled(TooltipStyle)`
   display: inline-block;
   margin-top: 5px;
   left: -37px;
@@ -184,7 +190,7 @@ const TooltipArrow = styled.div`
   height: 0;
 `
 
-const TooltipArrowLeft = styled(TooltipArrow)`
+export const TooltipArrowLeft = styled(TooltipArrow)`
   -webkit-filter: drop-shadow(1px 0px 1px rgba(0, 0, 0, 0.3));
   filter: drop-shadow(1px 0px 1px rgba(0, 0, 0, 0.3));
   border-top: 10px solid transparent;
@@ -193,7 +199,7 @@ const TooltipArrowLeft = styled(TooltipArrow)`
   margin-top: 10px;
 `
 
-const TooltipArrowRight = styled(TooltipArrow)`
+export const TooltipArrowRight = styled(TooltipArrow)`
   -webkit-filter: drop-shadow(-1px 0px 1px rgba(0, 0, 0, 0.3));
   filter: drop-shadow(-1px 0px 1px rgba(0, 0, 0, 0.3));
   border-top: 10px solid transparent;
@@ -202,7 +208,7 @@ const TooltipArrowRight = styled(TooltipArrow)`
   margin-top: 10px;
 `
 
-const TooltipArrowDown = styled(TooltipArrow)`
+export const TooltipArrowDown = styled(TooltipArrow)`
   -webkit-filter: drop-shadow(0px -1px 1px rgba(0, 0, 0, 0.3));
   filter: drop-shadow(0px -1px 1px rgba(0, 0, 0, 0.3));
   border-left: 10px solid transparent;
@@ -211,7 +217,7 @@ const TooltipArrowDown = styled(TooltipArrow)`
   margin-left: 10px;
 `
 
-const TooltipArrowUp = styled(TooltipArrow)`
+export const TooltipArrowUp = styled(TooltipArrow)`
   -webkit-filter: drop-shadow(0px 1px 1px rgba(0, 0, 0, 0.3));
   filter: drop-shadow(0px 1px 1px rgba(0, 0, 0, 0.2));
   border-left: 10px solid transparent;
