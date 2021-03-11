35c7a9a20c1015a1f21ed984abf6ba7eaa5114b3
Ismo Torvinen <ismo.torvinen@gmail.com>
2021-03-11 12:34:06 +0200

    fdwe script load guard, and using window.fdweRecordEvent instead of plain fdweRecordEvent

--- etsin_finder/frontend/js/stores/tracking/index.js
+++ etsin_finder/frontend/js/stores/tracking/index.js
@@ -5,19 +5,6 @@ class Matomo {
     this.scopeMetaTag = document.querySelector('meta[name="fdwe-scope"]')
   }
 
-  /*
-  loadMatomoWithScope = scope => {
-    const scopeMetaTag = document.createElement('meta')
-    scopeMetaTag.setAttribute('name', 'fdwe-scope')
-    scopeMetaTag.setAttribute('content', scope)
-    document.head.appendChild(scopeMetaTag)
-
-    const script = document.createElement('script')
-    script.setAttribute('src', 'https://matomo.fd-test.csc.fi/fdwe.js')
-    document.head.appendChild(script)
-    this.isMatomoLoaded = true
-  }
-*/
   changeService = service => {
     if (this.Env?.Flags?.flagEnabled('MATOMO_TRACKING')) {
       this.serviceMetaTag.setAttribute('content', service)
@@ -27,8 +14,7 @@ class Matomo {
   recordEvent = scope => {
     if (this.Env?.Flags?.flagEnabled('MATOMO_TRACKING')) {
       this.scopeMetaTag.setAttribute('content', scope)
-      // eslint-disable-next-line
-      fdweRecordEvent()
+      if (window.fdweRecordEvent) window.fdweRecordEvent()
     }
   }
 }
