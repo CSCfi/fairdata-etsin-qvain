7f9d233b1306237a18ce1fc28970b155f05a5f90
Ismo Torvinen <ismo.torvinen@gmail.com>
2021-03-11 12:24:35 +0200

    actor and auth tests

--- etsin_finder/frontend/js/components/qvain/utils/auth.js
+++ etsin_finder/frontend/js/components/qvain/utils/auth.js
@@ -1,16 +1,14 @@
 const devMetadataCreator = 'abc-user-123' // Teppo Testaaja
 
-export const checkLogin = (props) => {
+export const checkLogin = props => {
   const inDev = props.Stores.Env.environment === 'development'
   if (inDev) {
-    return new Promise((resolve) => {
-      resolve()
-    })
+    return Promise.resolve()
   }
   return props.Stores.Auth.checkLogin()
 }
 
-export const getUsername = (props) => {
+export const getUsername = props => {
   const inDev = props.Stores.Env.environment === 'development'
   if (inDev && isTeppo(props)) {
     return devMetadataCreator
@@ -18,4 +16,4 @@ export const getUsername = (props) => {
   return props.Stores.Auth.user.name
 }
 
-export const isTeppo = (props) => props.Stores.Auth.user.commonName === 'Teppo Testikäyttäjä'
+export const isTeppo = props => props.Stores.Auth.user.commonName === 'Teppo Testikäyttäjä'
