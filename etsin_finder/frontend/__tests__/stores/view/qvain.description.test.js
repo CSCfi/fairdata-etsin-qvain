cfacf9cd8fea832f03288e744a1be27ef07b781e
Ismo Torvinen <ismo.torvinen@gmail.com>
2021-03-30 14:45:19 +0300

    stores tests for title and description

---
+++ etsin_finder/frontend/__tests__/stores/view/qvain.description.test.js
@@ -0,0 +1,71 @@
+import Description from '../../../js/stores/view/qvain/qvain.description'
+import 'chai/register-expect'
+import { makeObservable } from 'mobx'
+
+jest.mock('../../../js/stores/view/qvain/qvain.multiLanguageField', () => {
+  class MultiLanguageField {
+    constructor(Parent) {
+      this.constructorFunc(Parent)
+    }
+
+    constructorFunc = jest.fn()
+
+    value = undefined
+  }
+  return MultiLanguageField
+})
+
+jest.mock('mobx', () => {
+  return {
+    ...jest.requireActual('mobx'),
+    makeObservable: jest.fn(),
+  }
+})
+
+describe('Description', () => {
+  describe('when calling constructor', () => {
+    let description
+    const Parent = { data: 'parent' }
+
+    beforeEach(() => {
+      description = new Description(Parent)
+    })
+
+    test('should call MultiLanguageField.constructor with parent', () => {
+      expect(description.constructorFunc).to.have.beenCalledWith(Parent)
+    })
+
+    test('should call makeObservbles', () => {
+      expect(makeObservable).to.have.beenCalledWith(description)
+    })
+
+    describe('when calling fromBackend with dataset', () => {
+      const dataset = {
+        description: {
+          fi: 'fi_desc',
+          en: 'en_desc',
+        },
+      }
+
+      beforeEach(() => {
+        description.fromBackend(dataset)
+      })
+
+      test('should assign value with dataset.description', () => {
+        description.value.should.eql({ en: 'en_desc', fi: 'fi_desc' })
+      })
+    })
+
+    describe('when calling fromBackend with empty dataset', () => {
+      const dataset = {}
+
+      beforeEach(() => {
+        description.fromBackend(dataset)
+      })
+
+      test('should assign value with dataset.description', () => {
+        description.value.should.eql({ fi: '', en: '' })
+      })
+    })
+  })
+})
