ffc604be0ae490b80c45e41ef6d8b574e45a6bcf
Ismo Torvinen <ismo.torvinen@gmail.com>
2021-05-10 13:39:31 +0300

    partial test suite for project

--- etsin_finder/frontend/__tests__/stores/view/qvain.project.test.js
+++ etsin_finder/frontend/__tests__/stores/view/qvain.project.test.js
@@ -1,8 +1,17 @@
 import 'chai/register-expect'
+import { v4 as uuidv4 } from 'uuid'
 import { makeObservable } from 'mobx'
 import Projects from '../../../js/stores/view/qvain/qvain.project'
 
-jest.mock('mobx')
+jest.mock('mobx', () => {
+  const actual = jest.requireActual('mobx')
+  return {
+    ...actual,
+    makeObservable: jest.fn(),
+  }
+})
+
+jest.mock('uuid')
 
 describe('Projects', () => {
   let projects
@@ -101,19 +110,252 @@ describe('Projects', () => {
         {
           name: { fi: 'fi-name' },
           identifier: 'identifier',
+          has_funder_identifier: 'funder_identifier',
           funder_type: {
             pref_label: 'name',
             identifier: 'url',
           },
-          source_organization: [{ name: 'name', identifier: 'identifier', email: 'email' }],
+          source_organization: [
+            {
+              name: 'subdep_name',
+              identifier: 'subdep_identifier',
+              email: 'subdep_email',
+              is_part_of: {
+                name: 'dep_name',
+                identifier: 'dep_identifier',
+                email: 'dep_email',
+                is_part_of: {
+                  name: 'org_name',
+                  identifier: 'org_identifier',
+                  email: 'org_email',
+                },
+              },
+            },
+          ],
+          has_funding_agency: [
+            {
+              name: 'fund_subdep_name',
+              identifier: 'fund_subdep_identifier',
+              email: 'fund_subdep_email',
+              is_part_of: {
+                name: 'fund_dep_name',
+                identifier: 'fund_dep_identifier',
+                email: 'fund_dep_email',
+                is_part_of: {
+                  name: 'fund_org_name',
+                  identifier: 'fund_org_identifier',
+                  email: 'fund_org_email',
+                },
+              },
+              contributor_type: [
+                {
+                  identifier: 'contr_identifier',
+                  pref_label: 'contr_pref_label',
+                  definition: 'contr_definition',
+                  in_scheme: 'contr_in_scheme',
+                },
+              ],
+            },
+          ],
         },
       ],
     }
 
     beforeEach(() => {
+      uuidv4.mockReturnValue('id')
       projects.fromBackend(dataset)
     })
 
-    test('', () => {})
+    test('should set projects with expected array', () => {
+      const expectedObj = {
+        id: 'id',
+        details: {
+          title: { fi: 'fi-name' },
+          identifier: 'identifier',
+          fundingIdentifier: 'funder_identifier',
+          funderType: { url: 'url', name: 'name' },
+        },
+        organizations: [
+          {
+            id: 'id',
+            organization: { name: 'org_name', identifier: 'org_identifier', email: 'org_email' },
+            department: { name: 'dep_name', identifier: 'dep_identifier', email: 'dep_email' },
+            subDepartment: {
+              name: 'subdep_name',
+              identifier: 'subdep_identifier',
+              email: 'subdep_email',
+            },
+          },
+        ],
+        fundingAgencies: [
+          {
+            id: 'id',
+            organization: {
+              id: 'id',
+              organization: {
+                name: 'fund_org_name',
+                identifier: 'fund_org_identifier',
+                email: 'fund_org_email',
+              },
+              department: {
+                name: 'fund_dep_name',
+                identifier: 'fund_dep_identifier',
+                email: 'fund_dep_email',
+              },
+              subDepartment: {
+                name: 'fund_subdep_name',
+                identifier: 'fund_subdep_identifier',
+                email: 'fund_subdep_email',
+              },
+            },
+            contributorTypes: [
+              {
+                id: 'id',
+                identifier: 'contr_identifier',
+                label: 'contr_pref_label',
+                definition: 'contr_definition',
+                inScheme: 'contr_in_scheme',
+              },
+            ],
+          },
+        ],
+      }
+
+      projects.projects.should.deep.eql([expectedObj])
+    })
+  })
+
+  describe('given existing project', () => {
+    const initialProjects = [
+      {
+        id: 'id',
+        details: {
+          title: { fi: 'fi-name' },
+          identifier: 'identifier',
+          fundingIdentifier: 'funder_identifier',
+          funderType: { url: 'url', name: 'name' },
+        },
+        organizations: [
+          {
+            id: 'id',
+            organization: { name: 'org_name', identifier: 'org_identifier', email: 'org_email' },
+            department: { name: 'dep_name', identifier: 'dep_identifier', email: 'dep_email' },
+            subDepartment: {
+              name: 'subdep_name',
+              identifier: 'subdep_identifier',
+              email: 'subdep_email',
+            },
+          },
+        ],
+        fundingAgencies: [
+          {
+            id: 'id',
+            organization: {
+              id: 'id',
+              organization: {
+                name: 'fund_org_name',
+                identifier: 'fund_org_identifier',
+                email: 'fund_org_email',
+              },
+              department: {
+                name: 'fund_dep_name',
+                identifier: 'fund_dep_identifier',
+                email: 'fund_dep_email',
+              },
+              subDepartment: {
+                name: 'fund_subdep_name',
+                identifier: 'fund_subdep_identifier',
+                email: 'fund_subdep_email',
+              },
+            },
+            contributorTypes: [
+              {
+                id: 'id',
+                identifier: 'contr_identifier',
+                label: 'contr_pref_label',
+                definition: 'contr_definition',
+                inScheme: 'contr_in_scheme',
+              },
+            ],
+          },
+        ],
+      },
+    ]
+
+    beforeEach(() => {
+      projects.projects = initialProjects
+    })
+
+    describe('when calling toBackend', () => {
+      let returnValue
+      beforeEach(() => {
+        returnValue = projects.toBackend()
+      })
+
+      test('should return backend ready array', () => {
+        const expectedReturn = [
+          {
+            details: {
+              funderType: { identifier: 'url' },
+              fundingIdentifier: 'funder_identifier',
+              identifier: 'identifier',
+              title: {
+                fi: 'fi-name',
+              },
+            },
+            fundingAgencies: [
+              {
+                contributorTypes: [
+                  {
+                    definition: 'contr_definition',
+                    identifier: 'contr_identifier',
+                    inScheme: 'contr_in_scheme',
+                    label: 'contr_pref_label',
+                  },
+                ],
+                organization: [
+                  {
+                    name: 'fund_org_name',
+                    identifier: 'fund_org_identifier',
+                    email: 'fund_org_email',
+                  },
+                  {
+                    name: 'fund_dep_name',
+                    identifier: 'fund_dep_identifier',
+                    email: 'fund_dep_email',
+                  },
+                  {
+                    name: 'fund_subdep_name',
+                    identifier: 'fund_subdep_identifier',
+                    email: 'fund_subdep_email',
+                  },
+                ],
+              },
+            ],
+            organizations: [
+              [
+                {
+                  name: 'org_name',
+                  identifier: 'org_identifier',
+                  email: 'org_email',
+                },
+                {
+                  name: 'dep_name',
+                  identifier: 'dep_identifier',
+                  email: 'dep_email',
+                },
+                {
+                  name: 'subdep_name',
+                  identifier: 'subdep_identifier',
+                  email: 'subdep_email',
+                },
+              ],
+            ],
+          },
+        ]
+
+        returnValue.should.deep.eql(expectedReturn)
+      })
+    })
   })
 })
