5c075e7890745b5b63fbbf8893c25e784f44ea4c
Jori Niemi <3295718+tahme@users.noreply.github.com>
2021-05-27 10:22:36 +0300

    CSCFAIRMETA-1044: Remove isTeppo function (#838)

--- etsin_finder/frontend/js/components/qvain/utils/auth.js
+++ etsin_finder/frontend/js/components/qvain/utils/auth.js
@@ -1,5 +1,3 @@
-const devMetadataCreator = 'abc-user-123' // Teppo Testaaja
-
 export const checkLogin = props => {
   const inDev = props.Stores.Env.environment === 'development'
   if (inDev) {
@@ -8,12 +6,4 @@ export const checkLogin = props => {
   return props.Stores.Auth.checkLogin()
 }
 
-export const getUsername = props => {
-  const inDev = props.Stores.Env.environment === 'development'
-  if (inDev && isTeppo(props)) {
-    return devMetadataCreator
-  }
-  return props.Stores.Auth.user.name
-}
-
-export const isTeppo = props => props.Stores.Auth.user.commonName === 'Teppo Testikäyttäjä'
+export const getUsername = props => props.Stores.Auth.user.name
