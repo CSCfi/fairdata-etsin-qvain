d95b30d2862bf3fb18bcd458b1ef12e39d3cdf10
Ismo Torvinen <ismo.torvinen@gmail.com>
2021-06-08 15:39:47 +0300

    remove button should be visible when editing rights and metadata has been added

--- etsin_finder/frontend/js/components/qvain/fields/files/ida/selectedItemsDropdown.jsx
+++ etsin_finder/frontend/js/components/qvain/fields/files/ida/selectedItemsDropdown.jsx
@@ -127,6 +127,7 @@ const EditDropdown = ({ item, parentArgs }) => {
     return (
       <BaseDropdown>
         <MetadataButton type="edit" />
+        <RemoveMetadataButton />
         {PasButtons()}
       </BaseDropdown>
     )
