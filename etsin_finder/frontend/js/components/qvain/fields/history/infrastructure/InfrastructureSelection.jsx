9397bc62699782091e82860d0bb3e5d97fc6c5db
Jori Niemi <3295718+tahme@users.noreply.github.com>
2021-07-27 09:32:02 +0300

    CSCFAIRMETA-1102: Show all infrastructure options in drop-down (#855)

--- etsin_finder/frontend/js/components/qvain/fields/history/infrastructure/InfrastructureSelection.jsx
+++ etsin_finder/frontend/js/components/qvain/fields/history/infrastructure/InfrastructureSelection.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { observer } from 'mobx-react'
-import Select from '../../../general/input/searchSelect'
+import Select from '../../../general/input/select'
 
 import { useStores } from '../../../utils/stores'
 
