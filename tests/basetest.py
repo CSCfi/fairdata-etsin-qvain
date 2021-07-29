985c4ccdfa063e7604154d45fc0fb771c4da1a09
Ismo Torvinen <ismo.torvinen@gmail.com>
2021-07-29 13:32:13 +0300

    tests for qvain utils

--- tests/basetest.py
+++ tests/basetest.py
@@ -18,15 +18,16 @@ from etsin_finder.app import create_app
 
 from .utils import get_test_catalog_record
 
+
 class FakeLoader(jinja2.BaseLoader):
     """Fake loader to avoid depending on index.html in tests"""
 
     def get_source(self, environment, template):
         """Return dummy template"""
-        return 'hello test', None, lambda: False
+        return "hello test", None, lambda: False
 
 
-class BaseTest():
+class BaseTest:
     """Use as base class for any tests. Contains fixtures and monkeypatched methods"""
 
     @pytest.fixture
@@ -50,19 +51,123 @@ class BaseTest():
         :return:
         """
         from etsin_finder.auth import authentication
-        monkeypatch.setattr(authentication, 'is_authenticated', lambda: True)
-        monkeypatch.setattr(authentication, 'is_authenticated_CSC_user', lambda: True)
+
+        monkeypatch.setattr(authentication, "is_authenticated", lambda: True)
+        monkeypatch.setattr(authentication, "is_authenticated_CSC_user", lambda: True)
 
         client = app.test_client()
         with client as c:
             with c.session_transaction() as sess:
-                sess['samlUserdata'] = {
-                    'urn:oid:1.3.6.1.4.1.16161.4.0.53': ['teppo_testaaja'],
-                    'urn:oid:2.5.4.42': ['Teppo'],
-                    'urn:oid:2.5.4.4': ['Testaaja'],
-                    'urn:oid:0.9.2342.19200300.100.1.3': ['teppo@yliopisto.fi']
+                sess["samlUserdata"] = {
+                    "urn:oid:1.3.6.1.4.1.16161.4.0.53": ["teppo_testaaja"],
+                    "urn:oid:2.5.4.42": ["Teppo"],
+                    "urn:oid:2.5.4.4": ["Testaaja"],
+                    "urn:oid:0.9.2342.19200300.100.1.3": ["teppo@yliopisto.fi"],
                 }
         return client
+
+    @pytest.fixture
+    def authd_no_user_name(self, monkeypatch):
+        """
+        User-authenticated Flask test client, CSC user
+
+        :param monkeypatch:
+        :return:
+        """
+        from etsin_finder.auth import authentication
+
+        monkeypatch.setattr(authentication, "is_authenticated", lambda: True)
+        monkeypatch.setattr(authentication, "get_user_csc_name", lambda: None)
+
+    @pytest.fixture
+    def user_details(self, monkeypatch):
+        """
+          CSC user details
+
+        :param monkeypatch:
+        :return:
+        """
+        from etsin_finder.auth import authentication
+
+        monkeypatch.setattr(authentication, "is_authenticated", lambda: True)
+        monkeypatch.setattr(
+            authentication, "get_user_csc_name", lambda: "teppo_testaaja"
+        )
+        monkeypatch.setattr(
+            authentication, "get_user_email", lambda: "teppo@yliopisto.fi"
+        )
+        monkeypatch.setattr(authentication, "get_user_firstname", lambda: "Teppo")
+        monkeypatch.setattr(authentication, "get_user_lastname", lambda: "Testaaja")
+
+        return {
+            "userid": "teppo_testaaja",
+            "email": "teppo@yliopisto.fi",
+            "name": "Teppo Testaaja",
+        }
+
+    @pytest.fixture
+    def user_123_details(self, monkeypatch):
+        """
+          CSC abc-user-123 details. The user is creator of test data
+
+        :param monkeypatch:
+        :return:
+        """
+        from etsin_finder.auth import authentication
+
+        monkeypatch.setattr(authentication, "is_authenticated", lambda: True)
+        monkeypatch.setattr(authentication, "get_user_csc_name", lambda: "abc-user-123")
+        monkeypatch.setattr(
+            authentication, "get_user_email", lambda: "test.user.123@yliopisto.fi"
+        )
+        monkeypatch.setattr(authentication, "get_user_firstname", lambda: "Test")
+        monkeypatch.setattr(authentication, "get_user_lastname", lambda: "User")
+
+        return {
+            "userid": "abc-user-123",
+            "email": "test.user.123@yliopisto.fi",
+            "name": "Test User",
+        }
+
+    @pytest.fixture
+    def no_IDA_projects(self, monkeypatch):
+        """
+        User with no IDA projects
+        """
+        from etsin_finder.auth import authentication
+
+        monkeypatch.setattr(authentication, "get_user_ida_projects", lambda: None)
+
+    @pytest.fixture
+    def IDA_project_info_missing(self, monkeypatch):
+        """
+        User with IDA projects information missing/faulty
+        """
+        from etsin_finder.auth import authentication
+
+        monkeypatch.setattr(authentication, "get_user_ida_projects", lambda: False)
+
+    @pytest.fixture
+    def IDA_projects_dont_match(self, monkeypatch):
+        """
+        User with IDA projects that don't match with files and directories
+        """
+        from etsin_finder.auth import authentication
+
+        monkeypatch.setattr(
+            authentication, "get_user_ida_projects", lambda: ["project_y"]
+        )
+
+    @pytest.fixture
+    def IDA_projects_ok(self, monkeypatch):
+        """
+        User with correct IDA project
+        """
+        from etsin_finder.auth import authentication
+
+        monkeypatch.setattr(
+            authentication, "get_user_ida_projects", lambda: ["project_x"]
+        )
 
     @pytest.fixture
     def unauthd_client(self, app, monkeypatch):
@@ -74,8 +179,9 @@ class BaseTest():
         :return:
         """
         from etsin_finder.auth import authentication
-        monkeypatch.setattr(authentication, 'is_authenticated', lambda: False)
-        monkeypatch.setattr(authentication, 'is_authenticated_CSC_user', lambda: False)
+
+        monkeypatch.setattr(authentication, "is_authenticated", lambda: False)
+        monkeypatch.setattr(authentication, "is_authenticated_CSC_user", lambda: False)
 
         client = app.test_client()
         return client
@@ -89,7 +195,8 @@ class BaseTest():
         :return:
         """
         from etsin_finder.services import cr_service
-        monkeypatch.setattr(cr_service, 'get_catalog_record', lambda x, y, z: None)
+
+        monkeypatch.setattr(cr_service, "get_catalog_record", lambda x, y, z=None: None)
 
     @pytest.fixture
     def open_catalog_record(self, monkeypatch):
@@ -100,7 +207,12 @@ class BaseTest():
         :return:
         """
         from etsin_finder.services import cr_service
-        monkeypatch.setattr(cr_service, 'get_catalog_record', lambda x, y, z: get_test_catalog_record('open'))
+
+        monkeypatch.setattr(
+            cr_service,
+            "get_catalog_record",
+            lambda x, y, z=None: get_test_catalog_record("open"),
+        )
 
     @pytest.fixture
     def login_catalog_record(self, monkeypatch):
@@ -111,7 +223,12 @@ class BaseTest():
         :return:
         """
         from etsin_finder.services import cr_service
-        monkeypatch.setattr(cr_service, 'get_catalog_record', lambda x, y, z: get_test_catalog_record('login'))
+
+        monkeypatch.setattr(
+            cr_service,
+            "get_catalog_record",
+            lambda x, y, z: get_test_catalog_record("login"),
+        )
 
     @pytest.fixture
     def permit_catalog_record(self, monkeypatch):
@@ -122,7 +239,12 @@ class BaseTest():
         :return:
         """
         from etsin_finder.services import cr_service
-        monkeypatch.setattr(cr_service, 'get_catalog_record', lambda x, y, z: get_test_catalog_record('permit'))
+
+        monkeypatch.setattr(
+            cr_service,
+            "get_catalog_record",
+            lambda x, y, z: get_test_catalog_record("permit"),
+        )
 
     @pytest.fixture
     def embargo_passed_catalog_record(self, monkeypatch):
@@ -133,7 +255,12 @@ class BaseTest():
         :return:
         """
         from etsin_finder.services import cr_service
-        monkeypatch.setattr(cr_service, 'get_catalog_record', lambda x, y, z: get_test_catalog_record('embargo', True))
+
+        monkeypatch.setattr(
+            cr_service,
+            "get_catalog_record",
+            lambda x, y, z: get_test_catalog_record("embargo", True),
+        )
 
     @pytest.fixture
     def embargo_not_passed_catalog_record(self, monkeypatch):
@@ -144,7 +271,12 @@ class BaseTest():
         :return:
         """
         from etsin_finder.services import cr_service
-        monkeypatch.setattr(cr_service, 'get_catalog_record', lambda x, y, z: get_test_catalog_record('embargo', False))
+
+        monkeypatch.setattr(
+            cr_service,
+            "get_catalog_record",
+            lambda x, y, z: get_test_catalog_record("embargo", False),
+        )
 
     @pytest.fixture
     def restricted_catalog_record(self, monkeypatch):
@@ -155,7 +287,31 @@ class BaseTest():
         :return:
         """
         from etsin_finder.services import cr_service
-        monkeypatch.setattr(cr_service, 'get_catalog_record', lambda x, y, z: get_test_catalog_record('restricted'))
+
+        monkeypatch.setattr(
+            cr_service,
+            "get_catalog_record",
+            lambda x, y, z: get_test_catalog_record("restricted"),
+        )
+
+    @pytest.fixture
+    def nonstandard_catalog_record(self, monkeypatch):
+        """
+        Some nonstandard catalog record
+
+        :param monkeypatch:
+        :return:
+        """
+        from etsin_finder.services import cr_service
+
+        monkeypatch.setattr(
+            cr_service,
+            "get_catalog_record",
+            lambda x, y, z=None: {
+                "metadata_provider_user": "teppo_testaaja",
+                "data_catalog": {"catalog_json": {"identifier": "nonstandard"}},
+            },
+        )
 
     @pytest.fixture
     def has_rems_permit(self, monkeypatch):
@@ -166,9 +322,17 @@ class BaseTest():
         :return:
         """
         from etsin_finder.services import rems_service
-        monkeypatch.setattr(rems_service, 'get_user_rems_permission_for_catalog_record', lambda x, y: True)
+
+        monkeypatch.setattr(
+            rems_service,
+            "get_user_rems_permission_for_catalog_record",
+            lambda x, y: True,
+        )
         from etsin_finder.auth import authorization
-        monkeypatch.setattr(authorization, 'user_has_rems_permission_for_catalog_record', lambda x: True)
+
+        monkeypatch.setattr(
+            authorization, "user_has_rems_permission_for_catalog_record", lambda x: True
+        )
 
     @pytest.fixture
     def no_rems_permit(self, monkeypatch):
@@ -179,11 +343,21 @@ class BaseTest():
         :return:
         """
         from etsin_finder.services import rems_service
-        monkeypatch.setattr(rems_service, 'get_user_rems_permission_for_catalog_record', lambda x, y: False)
-        from etsin_finder.auth import authorization
-        monkeypatch.setattr(authorization, 'user_has_rems_permission_for_catalog_record', lambda x: False)
 
-    if __name__ == '__main__':
+        monkeypatch.setattr(
+            rems_service,
+            "get_user_rems_permission_for_catalog_record",
+            lambda x, y: False,
+        )
+        from etsin_finder.auth import authorization
+
+        monkeypatch.setattr(
+            authorization,
+            "user_has_rems_permission_for_catalog_record",
+            lambda x: False,
+        )
+
+    if __name__ == "__main__":
         pytest.main()
 
     @pytest.fixture
@@ -204,11 +378,12 @@ class BaseTest():
             errors (list of str): Substrings expected in errors
 
         """
+
         def check(warnings=None, errors=None):
             warnings = warnings or []
             errors = errors or []
 
-            records = caplog.get_records('call')
+            records = caplog.get_records("call")
 
             log_warnings = [x.message for x in records if x.levelno == logging.WARNING]
             assert len(warnings) == len(log_warnings)
