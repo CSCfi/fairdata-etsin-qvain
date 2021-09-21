e1f621fa947e9e5a74f51ba211c374d7bc7fe9ab
Ismo Torvinen <ismo.torvinen@gmail.com>
2021-09-21 15:17:31 +0300

    cleanup

--- etsin_finder/frontend/js/stores/view/qvain/qvain.provenances.locations.js
+++ etsin_finder/frontend/js/stores/view/qvain/qvain.provenances.locations.js
@@ -3,14 +3,11 @@ import Spatials from './qvain.spatials'
 class Locations extends Spatials {
   constructor(Parent, locations = []) {
     super(Parent, locations)
-    console.log(locations)
   }
 
   clone = () => new Locations(this.Parent, this.toBackend())
 
   translationsRoot = 'qvain.history.provenance.modal.locationInput'
-
-  test = 'test'
 }
 
 export default Locations
