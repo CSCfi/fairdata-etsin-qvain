a295470a748108cbb5a9976d71f0e3552336481c
Ismo Torvinen <ismo.torvinen@gmail.com>
2021-09-21 14:47:06 +0300

    existing Locations in provenance missing translations fixed.

--- etsin_finder/frontend/js/stores/view/qvain/qvain.provenances.locations.js
+++ etsin_finder/frontend/js/stores/view/qvain/qvain.provenances.locations.js
@@ -1,13 +1,16 @@
 import Spatials from './qvain.spatials'
 
-class Locations extends Spatials
-{
-  constructor(Parent, locations = [])
-  {
+class Locations extends Spatials {
+  constructor(Parent, locations = []) {
     super(Parent, locations)
+    console.log(locations)
   }
 
+  clone = () => new Locations(this.Parent, this.toBackend())
+
   translationsRoot = 'qvain.history.provenance.modal.locationInput'
+
+  test = 'test'
 }
 
 export default Locations
