8194564a1e7e453a46a8e760bee05f468b899f0b
Ismo Torvinen <torvinen@csc.fi>
2025-02-07 11:48:43 +0000

    make access rights description field collapsable

--- etsin_finder/frontend/js/components/qvain/sections/DataOrigin/general/AccessRights/Description/index.jsx
+++ etsin_finder/frontend/js/components/qvain/sections/DataOrigin/general/AccessRights/Description/index.jsx
@@ -13,6 +13,7 @@ import {
 } from '@/components/qvain/general/V2'
 import TranslationTab from '@/components/qvain/general/V2/TranslationTab'
 import { ValidationError } from '@/components/qvain/general/errors'
+import { ExpandCollapse } from '@/components/qvain/general/V2/ExpandCollapse'
 
 const AccessRightsDescription = () => {
   const {
@@ -23,38 +24,52 @@ const AccessRightsDescription = () => {
     },
   } = useStores()
   const [language, setLanguage] = useState(lang)
+  const [isExpanded, setIsExpanded] = useState(Boolean(value?.fi || value?.en))
 
   const handleChange = e => set(e.target.value, language)
+
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded)
+  }
 
   const id = 'access-rights-description-input'
 
   return (
     <FieldGroup>
       <FieldWrapper>
-        <Translate component={TitleSmall} content="qvain.rightsAndLicenses.description.title" />
-        <TranslationTab language={language} setLanguage={setLanguage} id={id}>
+        <div>
+          <ExpandCollapse isExpanded={isExpanded} onClick={handleExpand} />
           <Translate
             component={TitleSmall}
             content="qvain.rightsAndLicenses.description.title"
-            htmlFor={id}
+            onClick={handleExpand}
           />
-          <Translate
-            component={NarrowTextArea}
-            id={id}
-            disabled={readonly}
-            value={value[language] || ''}
-            onChange={handleChange}
-            onBlur={validate}
-          />
-          <span>
+        </div>
+        {isExpanded && (
+          <TranslationTab language={language} setLanguage={setLanguage} id={id}>
             <Translate
-              component={InfoText}
-              id="accerss-rights-description-char-counter"
-              content="qvain.description.charactersRemaining"
-              with={{ charactersRemaining: charactersRemaining[language] }}
+              component={TitleSmall}
+              content="qvain.rightsAndLicenses.description.title"
+              htmlFor={id}
             />
-          </span>
-        </TranslationTab>
+            <Translate
+              component={NarrowTextArea}
+              id={id}
+              disabled={readonly}
+              value={value[language] || ''}
+              onChange={handleChange}
+              onBlur={validate}
+            />
+            <span>
+              <Translate
+                component={InfoText}
+                id="accerss-rights-description-char-counter"
+                content="qvain.description.charactersRemaining"
+                with={{ charactersRemaining: charactersRemaining[language] }}
+              />
+            </span>
+          </TranslationTab>
+        )}
       </FieldWrapper>
       {validationError && <ValidationError>{validationError}</ValidationError>}
     </FieldGroup>
